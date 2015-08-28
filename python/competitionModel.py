5f647cc02d59d7cd8bb03e4bb8ee179dcfcfbec3
Conor Lawless <cnr.lwlss@gmail.com>
2015-08-28 11:34:00 +0100

    Plotting both N and C (correctly)

--- python/competitionModel.py
+++ python/competitionModel.py
@@ -1,18 +1,18 @@
 from CANS_functions import *
 
 # Timepoints at which we would like to simulate cell densities (& remaining nutrients)
-t=np.linspace(0,15,100)
+t=np.linspace(0,30,100)
 
 nrow,ncol=3,3
 
 # Initial conditions
 C0=0.1
-N0=0.05
+N0=1.0
 
 # Parameter value
 r=[0.1,0.2,0.3,0.2,0.1,0.15,0.15,0.5,0.4]
 #r=[0.3 for x in range(0,nrow*ncol)]
-k=0.01
+k=0.5
 
 # Initial conditions
 C=[C0 for x in range(0,nrow*ncol)]
@@ -31,6 +31,7 @@ for row in range(nrow):
     for col in range(ncol):
         i=convertij((row,col),ncol)
         ax[row,col].plot(t,soln[:,i])
+        ax[row,col].plot(t,soln[:,i+(nrow*ncol)])
         ax[row,col].set_xlabel('Time since inoculation (d)')
         ax[row,col].set_ylabel('Population size (AU)')
 plt.show()
