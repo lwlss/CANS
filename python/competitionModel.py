8d4ba3665dd585aa38a374de6f3ae41dfced293d
Conor Lawless <cnr.lwlss@gmail.com>
2015-08-28 11:25:45 +0100

    Making array plot: separate plot for each spot

--- python/competitionModel.py
+++ python/competitionModel.py
@@ -7,15 +7,14 @@ nrow,ncol=3,3
 
 # Initial conditions
 C0=0.1
-N0=10.0
+N0=0.05
 
 # Parameter value
-#r=[0.1,0.2,0.3,0.2,0.1,0.15,0.15,0.5,0.4]
-r=[0.3 for x in range(0,nrow*ncol)]
-k=1
+r=[0.1,0.2,0.3,0.2,0.1,0.15,0.15,0.5,0.4]
+#r=[0.3 for x in range(0,nrow*ncol)]
+k=0.01
 
 # Initial conditions
-C0,N0=0.01,0.05
 C=[C0 for x in range(0,nrow*ncol)]
 N=[N0 for x in range(0,nrow*ncol)]
 
@@ -27,10 +26,12 @@ f=makeModelComp(nrow,ncol,r,k)
 soln=odeint(f,C+N,t)
 
 # Plotting results
-for i in range(0,nrow*ncol):
-    plt.plot(t,soln[:,i])
-plt.xlabel('Time since inoculation (d)')
-plt.ylabel('Population size (AU)')
-plt.title('Logistic growth')
+fig,ax=plt.subplots(nrow,ncol,figsize=(20,10))
+for row in range(nrow):
+    for col in range(ncol):
+        i=convertij((row,col),ncol)
+        ax[row,col].plot(t,soln[:,i])
+        ax[row,col].set_xlabel('Time since inoculation (d)')
+        ax[row,col].set_ylabel('Population size (AU)')
 plt.show()
 
