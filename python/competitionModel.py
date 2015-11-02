78c52bc3a7e48356655554ab215597d404ce9cd0
Conor Lawless <cnr.lwlss@gmail.com>
2015-11-02 16:59:57 +0000

    Plot data points
    Demonstration of plottting data points on the growth curves for the
    competition model.  Just looks at the top left 3 rows and 3 columns of
    the 384-format plate.  Middle spot grows strongly.  Selected 9 r
    parameter values, one k value and one N0 value by eye.

--- python/competitionModel.py
+++ python/competitionModel.py
@@ -1,18 +1,18 @@
 from CANS_functions import *
+import pandas as pd
 
 # Timepoints at which we would like to simulate cell densities (& remaining nutrients)
-t=np.linspace(0,30,100)
+t=np.linspace(0,4.5,100)
 
 nrow,ncol=3,3
 
 # Initial conditions
-C0=0.1
-N0=1.0
+C0=0.001
+N0=0.23
 
 # Parameter value
-r=[0.1,0.2,0.3,0.2,0.1,0.15,0.15,0.5,0.4]
-#r=[0.3 for x in range(0,nrow*ncol)]
-k=0.5
+r=[3,3,3,3,10,3,3,3,3]
+k=0.05
 
 # Initial conditions
 C=[C0 for x in range(0,nrow*ncol)]
@@ -25,13 +25,20 @@ f=makeModelComp(nrow,ncol,r,k)
 
 soln=odeint(f,C+N,t)
 
+# Read in some real data
+data=pd.read_csv("..\data\ColonyzerOutput.txt",sep="\t")
+
 # Plotting results
 fig,ax=plt.subplots(nrow,ncol,figsize=(20,10))
 for row in range(nrow):
     for col in range(ncol):
-        i=convertij((row,col),ncol)
-        ax[row,col].plot(t,soln[:,i])
-        ax[row,col].plot(t,soln[:,i+(nrow*ncol)])
+        dat=data[(data.Column==(col+1))&(data.Row==(row+1))]
+        i=convertij((row+1,col+1),ncol)
+        ax[row,col].scatter(dat.ExptTime,dat.Intensity,c="red")
+        ax[row,col].plot(t,soln[:,i],c="black")
+        ax[row,col].plot(t,soln[:,i+(nrow*ncol)],c="blue")
+        ax[row,col].set_ylim([-0.02,0.3])
+        ax[row,col].text(4, 0.275, 'R{r:02d}C{c:02d}'.format(r=row+1,c=col+1))
         ax[row,col].set_xlabel('Time since inoculation (d)')
         ax[row,col].set_ylabel('Population size (AU)')
 plt.show()
