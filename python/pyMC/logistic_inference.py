0a10c639f8f55b8e7683dc845c65a89bc4e651d5
Conor Lawless <conor.lawless@ncl.ac.uk>
2015-12-27 18:30:37 +0000

    Posterior predictive

--- python/pyMC/logistic_inference.py
+++ python/pyMC/logistic_inference.py
@@ -1,35 +1,48 @@
 import logistic_model
 from pymc import MCMC, Matplot
 import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy import stats
 from scipy.stats import gaussian_kde
 import numpy as np
 
-def gauss(x,mu=0.0,sigma=1.0):
-    return(1/(sigma * np.sqrt(2 * np.pi)) * np.exp( -(x - mu)**2 / (2*sigma**2)))
-
+def summarisePred(p,conf=0.95):
+    lowerperc=100.0*(1.0-conf)/2.0
+    upperperc=100.0-lowerperc
+    res={}
+    res['low']=[np.percentile(p.trace[:,i],lowerperc) for i in range(p.shape[0])]
+    res['med']=[np.percentile(p.trace[:,i],50.0) for i in range(p.shape[0])]
+    res['up']=[np.percentile(p.trace[:,i],upperperc) for i in range(p.shape[0])]
+    return(res)
+    
 def posteriorPriorPlots(M,bnum=50):
+
+    nodenames=[x.__name__ for x in M._variables_to_tally]
+    pred=summarisePred(M.pred)
     fig,ax=plt.subplots(2,3,figsize=(21,14))
+    
     ax[0,0].scatter(logistic_model.t_exp,logistic_model.x_exp)
     ax[0,0].set_xlabel('Time since inoculation (d)')
     ax[0,0].set_ylabel('Population size (AU)')
+
     ax[0,1].scatter(logistic_model.t_exp,logistic_model.x_obs)
+    ax[0,1].plot(logistic_model.t_pred,pred["med"],color="blue")
+    ax[0,1].plot(logistic_model.t_pred,pred["low"],color="blue",linestyle="dashed")
+    ax[0,1].plot(logistic_model.t_pred,pred["up"],color="blue",linestyle="dashed")
     ax[0,1].set_xlabel('Time since inoculation (d)')
     ax[0,1].set_ylabel('Population size (AU)')
-    
-    x0_pdf=gaussian_kde(M.trace('x0')[:])
-    x0_min,x0_max=np.percentile(M.trace('x0')[:],[0,100])
-    x0_range=np.linspace(x0_min,x0_max,500)
-    x0_dens=x0_pdf(x0_range)
-    ax[0,2].hist(M.trace('x0')[:],bins=np.linspace(logistic_model.x0_min,logistic_model.x0_max,bnum),normed=True,color="skyblue")
-    ax[0,2].plot(x0_range,x0_dens,'r',color="blue")
-    ax[0,2].vlines(logistic_model.x0_true,0,max(x0_dens),linestyles='dashed')
-    ax[0,2].set_xlabel('x0 (AU)')
-    ax[0,2].set_ylabel('Density')
-    yval=1.0/(logistic_model.x0_max-logistic_model.x0_min)
-    ax[0,2].vlines([logistic_model.x0_min,logistic_model.x0_max],0,yval,color="red")
-    ax[0,2].hlines(yval,logistic_model.x0_min,logistic_model.x0_max,color="red")
+
+    if 'x0' in nodenames: 
+        x0_pdf=gaussian_kde(M.trace('x0')[:])
+        x0_min,x0_max=np.percentile(M.trace('x0')[:],[0,100])
+        x0_range=np.linspace(x0_min,x0_max,500)
+        x0_dens=x0_pdf(x0_range)
+        ax[0,2].hist(M.trace('x0')[:],bins=np.linspace(logistic_model.x0_min,logistic_model.x0_max,bnum),normed=True,color="skyblue")
+        ax[0,2].plot(x0_range,x0_dens,'r',color="blue")
+        ax[0,2].vlines(logistic_model.x0_true,0,max(x0_dens),linestyles='dashed')
+        ax[0,2].set_xlabel('x0 (AU)')
+        ax[0,2].set_ylabel('Density')
+        yval=1.0/(logistic_model.x0_max-logistic_model.x0_min)
+        ax[0,2].vlines([logistic_model.x0_min,logistic_model.x0_max],0,yval,color="red")
+        ax[0,2].hlines(yval,logistic_model.x0_min,logistic_model.x0_max,color="red")
 
     r_pdf=gaussian_kde(M.trace('r')[:])
     r_min,r_max=np.percentile(M.trace('r')[:],[0,100])
@@ -73,13 +86,14 @@ def posteriorPriorPlots(M,bnum=50):
     plt.show()
 
 M = MCMC(logistic_model)
-M.sample(iter=25000, burn=1000, thin=50,progress_bar=False)
+M.sample(iter=250000, burn=10000, thin=100,progress_bar=False)
 Matplot.plot(M)
+
+predQC=False
+if predQC:
+    Matplot.plot(M.pred)
+    for i in range(0,M.pred.shape[0]):
+        Matplot.plot(M.pred.trace[:,i],"pred_%01d"%i)
+        
 posteriorPriorPlots(M,50)
-
-K_min,K_max=np.percentile(M.trace('K')[:],[0,100])
-K_range=np.linspace(K_min,K_max,500)
-K_prior=stats.norm(loc=logistic_model.K_mu,scale=np.sqrt(1.0/logistic_model.K_tau))
-K_prior.pdf(K_range)
-
 
--- python/pyMC/logistic_model.py
+++ python/pyMC/logistic_model.py
@@ -11,24 +11,36 @@ r_true=2.5
 x0_true=0.001
 tau_true=500.0
 t_exp=np.linspace(0,5,10)
+t_pred=np.linspace(0,max(t_exp),100)
 x_exp=logistic(x0_true,r_true,K_true,t_exp)
 x_obs=np.random.normal(x_exp,sqrt(1.0/tau_true))
 
-# Specify priors for model parameters (stochastic nodes)
+# Stochastic modes in the model (prior specifications)
 r_min,r_max=0.0,10.0
 r=Uniform('r',r_min,r_max)
 K_min,K_max=0.0,1.0
 K=Uniform('K',K_min,K_max)
-x0_min,x0_max=0.0,0.01
-x0=Uniform('x0',x0_min,x0_max)
+#x0_min,x0_max=0.0,0.1
+#x0=Uniform('x0',x0_min,x0_max)
 tau_min,tau_max=0,5000
 tau=Uniform('tau',tau_min,tau_max)
 
 # Deterministic nodes in the model (output from logistic model in this case)
+##@deterministic(plot=False)
+##def logisticobs(x0=x0,r=r,K=K):
+##    return(logistic(x0,r,K,t_exp))
+##
+##@deterministic(plot=False)
+##def logisticpred(x0=x0,r=r,K=K):
+##    return(logistic(x0,r,K,t_pred))
+
 @deterministic(plot=False)
-def logisticmodel(x0=x0,r=r,K=K):
-    return(logistic(x0,r,K,t_exp))
+def logisticobsfix(r=r,K=K):
+    return(logistic(x0_true,r,K,t_exp))
 
-obs=Normal('obs',mu=logisticmodel,tau=tau,value=x_obs,observed=True)
+@deterministic(plot=False)
+def logisticpredfix(r=r,K=K):
+    return(logistic(x0_true,r,K,t_pred))
 
-# Need to add function closure so that t_exp can be a variable
+pred=Normal('pred',mu=logisticpredfix,tau=tau)
+obs=Normal('obs',mu=logisticobsfix,tau=tau,value=x_obs,observed=True)
