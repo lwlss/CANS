b83bec429f16af5f15fde4b42b0300904c00d005
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-04-19 11:59:18 +0100

    Run a single culture simulation

--- cans/culture.py
+++ cans/culture.py
@@ -1,5 +1,11 @@
 """Clases for the representation of microbial cultures."""
 
+import numpy as np
+
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+
 class Culture:
     """A culture with an amount of cells, nutrients, and signal."""
     def __init__(self, cells=0.1, nutrients=1.0, signal=0.0):
@@ -17,3 +23,34 @@ class Culture:
         self.cells = cells
         self.nutrients = nutrients
         self.signal = signal
+
+
+    def growth(self, y, t, r, b, a):
+        """A growth model without diffusion."""
+        C, N, S = y
+        dydt = [r*N*C - b*S, -r*N*C, a*C]
+        return dydt
+
+    def sim_inde_growth(self, t):
+        y0 = [self.cells, self.nutrients, self.signal]
+        r = 1
+        b = 0.01
+        a = 0.01
+        sol = odeint(self.growth, y0, t, args=(r, b, a))
+        return sol
+
+    def plot_growth_sim(self):
+        t = np.linspace(0, 10, 101)
+        sol = self.sim_inde_growth(t)
+        plt.plot(t, sol[:, 0], 'b', label='cells')
+        plt.plot(t, sol[:, 1], 'y', label='nutrients')
+        plt.plot(t, sol[:, 2], 'r', label='signal')
+        plt.legend(loc='best')
+        plt.xlabel('t')
+        plt.grid()
+        plt.show()
+
+
+if __name__ == '__main__':
+    culture1 = Culture()
+    culture1.plot_growth_sim()
