f8db6a09d82231fa98f0718baf1b419236c168dc
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-04-19 11:59:50 +0100

    Initialise plate with cultures and check.

--- cans/plate.py
+++ cans/plate.py
@@ -1,5 +1,8 @@
 """Classes for the representation of agar plates."""
 
+from culture import Culture
+
+
 class Plate:
     """A simple agar plate containing cultures."""
     def __init__(self, rows=3, cols=3):
@@ -11,7 +14,26 @@ class Plate:
             Number of rows (default 3)
         cols : Optional[int]
             Number of columns (default 3)
+        cultures : list[Culture]
+            A list of rows*cols Culture objects.
         """
         self.rows = rows
         self.cols = cols
-        cultures = []
+        self.cultures = []
+        for culture in range(rows*cols):
+            self.cultures.append(Culture())
+
+
+    def sim_growth(self):
+        pass
+
+
+if __name__ == "__main__":
+    plate1 = Plate()
+    print(plate1.rows)
+    print(plate1.cols)
+    print(plate1.cultures)
+    print(plate1.cultures[0].cells)
+    print(plate1.cultures[0].nutrients)
+    print(plate1.cultures[0].signal)
+    print(len(plate1.cultures))
