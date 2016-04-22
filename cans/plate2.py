ae638f57ea9fe22d84c3d4b6b17a4901dc375abc
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-04-22 12:31:05 +0100

    Develop simulated plate class

--- cans/plate2.py
+++ cans/plate2.py
@@ -53,8 +53,10 @@ class Plate:
             Model parameters
         """
         neighbourhood = self.neighbourhood
+        # Separate out plate and culture level parameters.
         kn = params[0]
         ks = params[1]
+        culture_params = params[2:]    # Could also do the iter here.
         # Must have times as argument in cans_growth function as this
         # is a requirement of odeint?
         def cans_growth(amounts, times):
@@ -70,7 +72,7 @@ class Plate:
             S_diffusions = [sum([sig - signal[j] for j in neighbourhood[i]])
                             for i, sig in enumerate(signal)]
             # An iterator of values for variables/terms appearing in the model.
-            vals = zip(*[iter(y)]*3, *[iter(params[2:])]*3, N_diffusions, S_diffusions)
+            vals = zip(*[iter(y)]*3, *[iter(culture_params)]*3, N_diffusions, S_diffusions)
             # This will sometimes store a negative amounts. This can
             # be corrected in the results returned by odeint if call
             # values are ALSO set to zero at the start of each
@@ -86,10 +88,42 @@ class Plate:
         pass
 
 
+# Can do this with or without using culture classes. Going to stick
+# with Culture classes because we may more easily adapt code to
+# simulate a plate of cultures that have different parameter
+# distributions. This could be useful when testing fitting methods for
+# fixing certain parameters on some plates.
 class SimPlate(Plate):
 
-    def __init__(self, rows=3, cols=3, kn=None, ks=None):
+    def __init__(self, rows=3, cols=3, kn=0.1, ks=0.1):
         # Call Plate __init__
-        super(SimPlate, self).__init__(rows=3, cols=3, kn=None, ks=None)
+        super(SimPlate, self).__init__(rows, cols, kn, ks)
         # Then also fill the plate with RandomCultures.
         self.cultures = [RandomCulture() for i in range(self.no_cultures)]
+        self.true_params = [kn, ks] + self.collect_params()
+
+
+    def collect_init_vals(self):
+        """Collect a list of initial values for each culture.
+
+        Return a flattened list of cell, nutirient, and signal amounts for
+        each culture. I.e. [C0, N0, S0, C1, N1, S1, ...].
+        """
+        init_vals = [val for culture in self.cultures
+                     for val in (culture.cells, culture.nutrients,
+                                 culture.signal)]
+        return init_vals
+
+
+    def collect_params(self):
+        """Collect parameters for each culture.
+
+        Return a flattened list of r, b, and a constants for each culture.
+        """
+        culture_params = [param for culture in self.cultures
+                          for param in (culture.r, culture.b, culture.a)]
+        params = [self.kn, self.ks] + culture_params
+        return params
+
+
+    # Now run and plot those simulations
