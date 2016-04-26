1c67378dff8bc14d43979bca440f7479f27714af
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-04-26 18:50:16 +0100

    Replace plate.py with plate2.py

--- cans/cans/plate.py
+++ cans/cans/plate.py
@@ -1,78 +1,35 @@
-"""Classes for the representation of agar plates."""
-
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
 
 
 from scipy.integrate import odeint
+from scipy.optimize import minimize
 
 
-from culture import Culture, RandomCulture
+from culture import RandomCulture
 
 
 class Plate:
-    """An agar plate containing cultures."""
-    def __init__(self, rows=3, cols=3, kn=0.0, ks=0.0):
-        """Initialise plate with an array of cultures.
-
-        Parameters
-        ----------
-        rows : Optional[int]
-            Number of rows (default 3)
-        cols : Optional[int]
-            Number of columns (default 3)
-        kn : Optional[float]
-            Nutrient diffusion constant (default 0.0)
-        ks : Optional[float]
-            Signal diffusion constant (default 0.0)
-        cultures : list[Culture]
-            A list of rows*cols Culture objects.
-        """
+    def __init__(self, rows=3, cols=3, kn=None, ks=None):
         self.rows = rows
         self.cols = cols
+        self.no_cultures = rows*cols
+        self.neighbourhood = self.find_neighbourhood()
         self.kn = kn
         self.ks = ks
-        self.cultures = self.get_cultures()
-        self.neighbourhood = self.find_neighbourhood()
+        # self.init_params
+        # self.init_amounts = None
+        # self.init_guess
+        # self.times = times
 
-
-    def get_cultures(self):
-        """Populate plate with cultures with the same parameters."""
-        return [Culture() for culture in range(self.rows*self.cols)]
-
-
-    def collect_init_vals(self):
-        """Collect a list of initial values for each culture.
-
-        Return a flattened list of cell, nutirient, and signal amounts for
-        each culture. I.e. [C0, N0, S0, C1, N1, S1, ...].
-        """
-        init_vals = [val for culture in self.cultures
-                     for val in (culture.cells, culture.nutrients,
-                                 culture.signal)]
-        return init_vals
-
-
-    def collect_params(self):
-        """Collect parameters for each culture.
-
-        Return a flattened list of r, b, and a constants for each culture.
-
-        """
-        culture_params = [param for culture in self.cultures
-                          for param in (culture.r, culture.b, culture.a)]
-        params = [self.kn, self.ks] + culture_params
-        return params
 
 
     def find_neighbourhood(self):
         """Return a list of tuples of neighbour indices for each culture."""
         rows = self.rows
         cols = self.cols
-        no_cultures = rows*cols
         neighbourhood = []
-        for i in range(no_cultures):
+        for i in range(self.no_cultures):
             neighbours = []
             if i // cols:
                 # Then not in first row.
@@ -90,122 +47,63 @@ class Plate:
         return neighbourhood
 
 
-    def inde_growth(self, y, t, params):
-        """Return independent odes for each culture."""
-        # Cannot have negative cell numbers or concentrations
-        np.maximum(0, y, out=y)
-        # The zip reapeats the same interator thrice so as to group y by
-        # threes. This is a Python idiom.
-        rates = [rate for C, N, S, r, b, a in zip(*[iter(y)]*3, *[iter(params[2])]*3)
-                 for rate in (r*N*C - b*S, -r*N*C, a*C)]    # *b*S*C?
-        return rates
+    def make_cans_model(self, params):
+        """Return a function for running the cans model.
 
-
-    def sim_inde_growth(self, t):
-        """Simulate independent growth for ."""
-        init_vals = self.collect_init_vals()
-        params = self.collect_params()
-        sol = odeint(self.inde_growth, init_vals, t, args=(params,))
-        # Remove negative amounts.
-        np.maximum(0, sol, out=sol)
-        return sol
-
-
-    def plot_inde_sims(self, filename='inde.pdf'):
-        """Plot growth curves for each culture."""
-        t = np.linspace(0, 15, 151)
-        sol = self.sim_inde_growth(t)
-        fig = plt.figure()
-        for i in range(self.rows*self.cols):
-            fig.add_subplot(self.rows, self.cols, i+1)
-            plt.plot(t, sol[:, i*3], 'b', label='cells')
-            plt.plot(t, sol[:, i*3 + 1], 'y', label='nutrients')
-            plt.plot(t, sol[:, i*3 + 2], 'r', label='signal')
-            plt.xlabel('t')
-            plt.grid()
-        if filename is None:
-            plt.show()
-        else:
-            # plt.legend(loc='best')
-            plt.savefig(filename)
-
-
-    def cans_growth(self, y, t, params, neighbourhood):
-        """Return independent odes for each culture.
-
-        y contains a list of cells, nutrients, and signal... repeated
-        for each culture.
-
+        Args
+        ----
+        params : list
+            Model parameters
         """
-        # Cannot have negative cell numbers or concentrations. If we
-        # set this here then we may still get negative values for
-        # amounts in the solution but these can be replaced without
-        # having any side effects.
-        np.maximum(0, y, out=y)
-        try:
-            assert(min(y) >= 0)
-        except AssertionError:
-            print("y contains C, N, or S less than zero.")
-        # Amounts of nutrients and signal.
-        nutrients = y[1::3]
-        signal = y[2::3]
-        # Sums of nutrient and signal diffusion for each culture.
-        N_diffusions = [sum([nutrient - nutrients[j] for j in neighbourhood[i]])
-                        for i, nutrient in enumerate(nutrients)]
-        S_diffusions = [sum([sig - signal[j] for j in neighbourhood[i]])
-                        for i, sig in enumerate(signal)]
+        neighbourhood = self.neighbourhood
+        # Separate out plate and culture level parameters.
         kn = params[0]
         ks = params[1]
-        # An iterator of values for variables/terms appearing in the model.
-        vals = zip(*[iter(y)]*3, *[iter(params[2:])]*3, N_diffusions, S_diffusions)
-        # This will sometimes store a negative amounts. This can be
-        # corrected in the results returned by odeint if at the start
-        # of each function call values are set to zero (see
-        # np.maximum() above).
-        rates = [rate for C, N, S, r, b, a, Ndiff, Sdiff in vals for rate in
-                (r*N*C - b*S, -r*N*C - kn*Ndiff, a*C - ks*Sdiff)]
-        return rates
+        culture_params = params[2:]
+        # Must have times as argument in cans_growth function as this
+        # is a requirement of odeint?
+        def cans_growth(amounts, times):
+            """Return cans rates given current amounts and times."""
+            # Cannot have negative amounts.
+            np.maximum(0, amounts, out=amounts)
+            # Amounts of nutrients and signal.
+            nutrients = amounts[1::3]
+            signal = amounts[2::3]
+            # Sums of nutrient and signal diffusion for each culture.
+            N_diffusions = [sum([nutrient - nutrients[j] for j in neighbourhood[i]])
+                            for i, nutrient in enumerate(nutrients)]
+            S_diffusions = [sum([sig - signal[j] for j in neighbourhood[i]])
+                            for i, sig in enumerate(signal)]
+            # An iterator of values for variables/terms appearing in the model.
+            vals = zip(*[iter(amounts)]*3, *[iter(culture_params)]*3,
+                       N_diffusions, S_diffusions)
+            # This will sometimes store negative amounts. This can
+            # be corrected in the results returned by odeint if call
+            # values are ALSO set to zero at the start of each
+            # function call (see np.maximum() above).
+            rates = [rate for C, N, S, r, b, a, Ndiff, Sdiff in vals for rate
+                     in (r*N*C - b*S*C, -r*N*C - kn*Ndiff, r*N*C - ks*Sdiff)]
+            return rates
+        return cans_growth
 
 
-    def sim_cans_growth(self, t, full_output=False):
-        """Simulate cans growth for whole plate."""
-        init_vals = self.collect_init_vals()
-        # can I just define params and neighbourhood in this scope and
-        # not have to pass them to odeint? Conor acheives this by
-        # defining the ode function within another function which
-        # returns the inner function. Maybe odeint deals with args in
-        # a clever way though so that they are not passes every time.
-        # Also consider moviing growth models to a different module.
-        params = self.collect_params()
-        neighbourhood = self.find_neighbourhood()
-        sol = odeint(self.cans_growth, init_vals, t,
-                     args=(params, neighbourhood), full_output=full_output)
-        # Remove negative amounts.
-        np.maximum(0, sol[0], out=sol[0])
-        print("Solved")
-        return sol
+    def solve_model(self, init_amounts, params):
+        # init_amounts should be an array of length 3*no_cultures.
+        growth_func = self.make_cans_model(params)
+        sol = odeint(growth_func, init_amounts, self.times)
+        return np.maximum(0, sol)
 
 
-    def sim_cans_growth2(self, t, init_amounts, params):
-        sol = odeint(self.cans_growth, init_amounts, t,
-                     args=(params, self.neighbourhood))
-        # Remove negative amounts.
-        np.maximum(0, sol, out=sol)
-        print("Solved")
-        return sol
-
-
-
-    def plot_cans_sims(self, filename='cans.pdf'):
-        """Plot growth curves for each culture."""
-        t = np.linspace(0, 15, 151)
-        sol = self.sim_cans_growth(t)
+    # Should work for simulations, fits, and experimental data.
+    # Times used by odeint can be returned as 'tcur' in the info dict
+    # if full_output is set to True.
+    def plot_growth(self, amounts, filename=None):
         fig = plt.figure()
-        for i in range(self.rows*self.cols):
+        for i in range(self.no_cultures):
             fig.add_subplot(self.rows, self.cols, i+1)
-            plt.plot(t, sol[:, i*3], 'b', label='cells')
-            plt.plot(t, sol[:, i*3 + 1], 'y', label='nutrients')
-            plt.plot(t, sol[:, i*3 + 2], 'r', label='signal')
+            plt.plot(self.times, amounts[:, i*3], 'b', label='cells')
+            plt.plot(self.times, amounts[:, i*3 + 1], 'y', label='nutrients')
+            plt.plot(self.times, amounts[:, i*3 + 2], 'r', label='signal')
             plt.xlabel('t')
             plt.grid()
         if filename is None:
@@ -215,116 +113,108 @@ class Plate:
             plt.savefig(filename)
 
 
-    def fit_cans(self):
-        # Use a simulated data set as observed values.
-        t = np.linspace(0, 15, 16)
-        data, info = self.sim_cans_growth(t, full_output=True)
-        obs_cells = [data[:, i*3] for i in range(self.rows*self.cols)]
-        obs_times = [0.0] + list(info['tcur'])
-        obs = (obs_cells, obs_times)
-        # List of initial variable and parameter guesses.
-        # Initial amounts are experimentally controlled (known) parameters.
-        C0 = 0.1
-        N0 = 1.0
-        S0 = 0.0
-        init_amounts = [init_val for i in range(self.rows*self.cols)
-                        for init_val in (C0, N0, S0)]
-        # Diffusion rates are the same for all but not certain.
-        kn = 0.1
-        ks = 0.1
-        # These are initial guesses of parameters that are different
-        # for all cultures.
-        r = 1.0
-        b = 0.1
-        a = 0.1
-        culture_params = [param for i in range(self.rows*self.cols)
-                          for param in (r, b, a)]
-        # List of initial estimates in form to allow fitting.
-        ests = [kn, ks] + culture_params
-        # Options for minimization
-        opts = {'disp': False, 'gtol': 1e-02, 'eps': 0.0001,
-                   'return_all': False, 'maxiter': 1000, 'norm': np.inf}
-        # Minimisation
-        est_params = opt.minimize(self.least_squares, ests,
-                                  args=(init_amounts,) + obs,
-                                  method='BFGS', jac=None, tol=None,
-                                  callback=None, options=opts)
-        return est_params, init_amounts, obs_times
+    def obj_func(self, params):
+        # Could do tiling later in solve_model if faster.
+        init_amounts = np.tile(params[: 3], self.no_cultures)
+        params = params[3:]
+        # Now find the amounts from simulations using the parameters.
+        amounts_est = self.solve_model(init_amounts, params)
+        c_est = np.array([amounts_est[:, i*3] for i in range(self.no_cultures)]).flatten()
+        err = np.sqrt(sum((self.c_meas - c_est)**2))
+        return err
 
 
-    def sum_squares(self, est_cells, obs_cells):
-        """Sum of squares of estimated and observerd cell diffierences."""
-        # Sum of element-wise subtraction and square.
-        sum_of_squares = sum(np.square(np.subtract(est_cells,  obs_cells)))
-        print(type(sum_of_squares))
-        return sum_of_squares
+    def init_guess(self):
+        """Return an initial parameter guess."""
+        # C(t=0), N(t=0), S(t=0)
+        amounts_guess = [0.2, 0.2, 0.0]
+        # kn, ks
+        kn_ks_guess = [0.05, 0.15]
+        # r, b, a
+        rates_guess = [1.0, 0.05, 0.05]
+        # Initial guess: C(t=0), N(t=0), S(t=0), kn, ks, r0, b0, a0, r1, b1, a1, ...
+        init_guess = np.array(amounts_guess + kn_ks_guess
+                              + rates_guess*self.no_cultures)
+        return init_guess
 
 
-    def least_squares(self, ests, init_amounts, obs_cells, obs_times):
-        # simulate growth from estimated parameters.
-        print(obs_times)
-        est_growth = odeint(self.cans_growth, init_amounts, obs_times,
-                            args=(ests, self.neighbourhood), full_output=False)
-        # Remove negative amounts.
-        np.maximum(0, est_growth[0], out=est_growth)
-        est_cells = [est_growth[:, i*3] for i in range(self.rows*self.cols)]
-        # calculate the sum of squares and return values.
-        sum_of_squares = self.sum_squares(est_cells, obs_cells)
-        return sum_of_squares
 
 
-    def plot_fit(self, filename='fit.pdf'):
-        # plot a fit using the estimated parameters.
-        params, init_amounts, t = self.fit_cans()
-        # If I reorder these I could just unpack with *args.
-        sol = self.sim_cans_growth2(t, init_amounts, params)
-        fig = plt.figure()
-        for i in range(self.rows*self.cols):
-            fig.add_subplot(self.rows, self.cols, i+1)
-            plt.plot(t, sol[:, i*3], 'b', label='cells')
-            plt.plot(t, sol[:, i*3 + 1], 'y', label='nutrients')
-            plt.plot(t, sol[:, i*3 + 2], 'r', label='signal')
-            plt.xlabel('t')
-            plt.grid()
-        if filename is None:
-            plt.show()
-        else:
-            # plt.legend(loc='best')
-            plt.savefig(filename)
+# Can do this with or without using culture classes. Going to stick
+# with Culture classes because we may more easily adapt code to
+# simulate a plate of cultures that have different parameter
+# distributions. This could be useful when testing fitting methods for
+# fixing certain parameters on some plates.
+class SimPlate(Plate):
+
+    def __init__(self, rows=3, cols=3, kn=0.1, ks=0.1, times=None):
+        # Call Plate __init__
+        super(SimPlate, self).__init__(rows, cols, kn, ks)
+        # Then also fill the plate with RandomCultures.
+        self.cultures = [RandomCulture() for i in range(self.no_cultures)]
+        self.true_init_amounts = self.collect_init_amounts()
+        self.true_params = self.collect_params()
+        self.times = times    # Can be taken from the data for a plate.
+        # Artificial data including hidden varibles.
+        self.true_amounts = self.solve_model(self.true_init_amounts,
+                                             self.true_params)
+        # Cells times-course. The data from a real plate.
+        self.c_meas = np.array([self.true_amounts[:, i*3] for i in
+                                range(self.no_cultures)]).flatten()
 
 
-class RandomPlate(Plate):
-    """A plate containing cultures with randomised parameters."""
+    def collect_init_amounts(self):
+        """Collect a list of initial values for each culture.
 
-    def get_cultures(self):
-        """Return a list of cultures with random parameters."""
-        # This is called in __init__.
-        return [RandomCulture() for culture in range(self.rows*self.cols)]
+        Return a flattened list of cell, nutirient, and signal amounts for
+        each culture. I.e. [C0, N0, S0, C1, N1, S1, ...].
+        """
+        init_amounts = [amount for culture in self.cultures for amount in
+                        (culture.cells, culture.nutrients, culture.signal)]
+        return init_amounts
 
 
-if __name__ == "__main__":
-#     plate1 = Plate()
-#     print(plate1.rows)
-#     print(plate1.cols)
-#     print(plate1.cultures)
-#     print(plate1.cultures[0].cells)
-#     print(plate1.cultures[0].nutrients)
-#     print(plate1.cultures[0].signal)
-#     print(len(plate1.cultures))
-#     print(plate1.collect_init_vals())
-#     print(len(plate1.collect_init_vals()))
-#     print(plate1.collect_params())
-#     print(len(plate1.collect_params()))
-# #    plate1.plot_growth_sims()
-#     print(plate1.find_neighbourhood())
-#    plate1.plot_cans_sims()
-    rand_plate = RandomPlate(3, 3, kn=0.1, ks=0.1)
-#    t = np.linspace(0, 15, 151)
-#    rand_plate.sim_cans_growth(t)
-#    rand_plate.plot_cans_sims()
- #   rand_plate.plot_inde_sims()
-    # rand_plate.plot_cans_sims(t)
-    # rand_plate.kn = 0.0
-    # rand_plate.ks = 0.0
-    # rand_plate.plot_cans_sims('inde.pdf')
-    rand_plate.plot_fit()
+    def collect_params(self):
+        """Collect parameters for each culture.
+
+        Return a flattened list of r, b, and a constants for each culture.
+        """
+        culture_params = [param for culture in self.cultures
+                          for param in (culture.r, culture.b, culture.a)]
+        params = [self.kn, self.ks] + culture_params
+        return params
+
+
+    def fit_data(self, plot=True):
+        init_guess = self.init_guess()
+        bounds = [(0.0, None) for i in range(len(init_guess))]
+        bounds[2] = (0.0, 0.0)    # S(t=0), remove from params and
+                                  # instead set const.
+        res = minimize(self.obj_func, init_guess, method='L-BFGS-B',
+                       bounds=bounds, options={'disp': True})
+        est_amounts = self.solve_model(np.tile(res.x[: 3], self.no_cultures),
+                                       res.x[3 :])
+        if plot:
+            self.plot_growth(self.true_amounts, filename='true.pdf')
+            self.plot_growth(est_amounts, filename='est.pdf')
+        return est_amounts
+
+
+if __name__ == '__main__':
+    # Initialize a plate filled with random cultures.
+    times = np.linspace(0, 20, 201)
+    sim1 = SimPlate(3, 3, times=times)
+    # Set/collect arguments for simulation.
+    cans_true_params = sim1.collect_params()
+    # Simulate and save plot of cans growth.
+    cans_sol = sim1.true_amounts
+    sim1.plot_growth(cans_sol, filename='cans_growth.pdf')
+    # Now make independent by setting diffusion parameters to zero,
+    sim1.kn = 0.0
+    sim1.ks = 0.0
+    init_amounts = sim1.collect_init_amounts()
+    # Careful not to use self.true_params which contains old kn, ks.
+    inde_true_params = sim1.collect_params()
+    # and simulate again
+    inde_sol = sim1.solve_model(init_amounts, inde_true_params)
+    sim1.plot_growth(inde_sol, filename='inde_growth.pdf')
