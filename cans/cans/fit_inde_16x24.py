846a50e364c5ed484f26b7f3300adf38d7883979
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-04 20:29:11 +0100

    Ready for first run with maxiter 1000 and None

--- cans/cans/fit_inde_16x24.py
+++ cans/cans/fit_inde_16x24.py
@@ -1,11 +1,14 @@
+"""Independent fit of a 16x24 comp sim with kn=0."""
 import csv
 import numpy as np
 import json
 
-from cans import find_neighbourhood
-import inde
 
+import inde
+import competition as comp
 from fitting import calc_devs
+from cans import find_neighbourhood
+
 
 # Read in same 16x24 data as before for kn=0
 # This time use stricter stopping criteria for independent model.
@@ -14,7 +17,7 @@ cols = 24
 no_cultures = rows*cols
 neighbourhood = find_neighbourhood(rows, cols)
 times = np.linspace(0, 15, 21)
-dir_name = 'results/comp_sim_fits_vary_kn_16x24/'
+dir_name = 'results/comp_sim_fits_vary_kn_16x24/strict_stop/'
 
 param_file = dir_name + 'sim_0_true_params.csv'
 with open(param_file, 'r') as f:
@@ -25,26 +28,26 @@ init_amounts = np.tile(true_params[:2], no_cultures)
 true_amounts = comp.solve_model(init_amounts, times,
                                 neighbourhood, true_params[2:])
 
-inde_param_est = inde.fit_model(rows, cols, times, true_amounts)
-inde_param_est = np.insert(inde_param_est.x, 2, np.nan)
+for maxiter in (1000, None):
+    inde_param_est = inde.fit_model(rows, cols, times, true_amounts)
+    no_iters = inde_param_est.nit
+    inde_param_est = np.insert(inde_param_est.x, 2, np.nan)
 
-inde_devs = calc_devs(true_params, 3, inde_param_est)[0]
+    inde_devs = calc_devs(true_params, 3, inde_param_est)[0]
 
-data = {
-    'kn=0 refitting' :
-    {
+    data = {
         'true_params' : true_params,
-        'inde_est' : inde_param_est.x,
-        'inde_devs' : inde_devs
+        'inde_est' : inde_param_est,
+        'inde_devs' : inde_devs,
+        'no_iters' : no_iters
     }
-}
-# Json requires np.ndarray to be dumped as a list.
-for k, v in data.items():
-    if isinstance(v, np.ndarray):
-        data[k] = v.tolist()
 
-with open(dir_name + 'inde_param_devs_0_strict_stop.json', 'w') as f:
-    json.dump(data, f, sort_keys=True, indent=4)
+    # Json requires np.ndarray to be dumped as a list.
+    for k, v in data.items():
+        if isinstance(v, np.ndarray):
+            data[k] = v.tolist()
 
-
-
+    data_file = (dir_name
+                 + 'inde_param_devs_0_strict_stop_{}.json'.format(maxiter))
+    with open(data_file, 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=4)
