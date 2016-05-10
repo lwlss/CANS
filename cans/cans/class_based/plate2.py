d4b2af85e8cc0927a016895c07df2e8079ab83e6
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-10 13:45:28 +0100

    Correct indent and pass self to methods

--- cans/cans/class_based/plate2.py
+++ cans/cans/class_based/plate2.py
@@ -1,6 +1,5 @@
 class Plate:
-
-    def __init__(rows, cols, data=None):
+    def __init__(self, rows, cols, data=None):
         self.rows = rows
         self.cols = cols
         self.no_cultures = rows*cols
@@ -9,10 +8,10 @@ class Plate:
         # Is data going to be a dictionary of observation times and
         # cell measurements? Let's assume so.
         if data is not None:
-            self.c_meas = data['c_meas']
+            self.c_meas = data['c_meas']    # Should flatten as np.array
             self.times = data['times']
         else:
-            self.c_meas = None
+            self.c_meas = None    # Should flatten as np.array
             self.times = None
         # Specicial attributes for simulated data.
         self.sim_amounts = None
@@ -20,22 +19,33 @@ class Plate:
         # A plate has Cultures
 
 
-    def find_neighbourhood():
-    """Return a list of tuples of neighbour indices for each culture."""
-    neighbourhood = []
-    for i in range(self.no_cultures):
-        neighbours = []
-        if i // self.cols:
-            # Then not in first row.
-            neighbours.append(i - self.cols)
-        if i % self.cols:
-            # Then not in first column.
-            neighbours.append(i - 1)
-        if (i + 1) % self.cols:
-            # Then not in last column.
-            neighbours.append(i + 1)
-        if i < (self.rows - 1 )*self.cols:
-            # Then not in last row.
-            neighbours.append(i + self.cols)
-        neighbourhood.append(tuple(neighbours))
-    return neighbourhood
+    def find_neighbourhood(self):
+        """Return a list of tuples of neighbour indices for each culture."""
+        neighbourhood = []
+        for i in range(self.no_cultures):
+            neighbours = []
+            if i // self.cols:
+                # Then not in first row.
+                neighbours.append(i - self.cols)
+            if i % self.cols:
+                # Then not in first column.
+                neighbours.append(i - 1)
+            if (i + 1) % self.cols:
+                # Then not in last column.
+                neighbours.append(i + 1)
+            if i < (self.rows - 1 )*self.cols:
+                # Then not in last row.
+                neighbours.append(i + self.cols)
+            neighbourhood.append(tuple(neighbours))
+        return neighbourhood
+
+
+class Culture(Plate):
+    def __init__(self, data=None):
+        super(Culture, self).__init__(1, 1, data)
+
+if __name__ == '__main__':
+    plate1 = Plate(3, 3)
+    culture1 = Culture()
+    print(plate1.neighbourhood)
+    print(culture1.neighbourhood)
