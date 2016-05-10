191acd7273e4633f982dd24e4b38cf193936fdf6
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-10 17:58:53 +0100

    Fit each culture independently and store estimate.

--- cans/cans/class_based/test_classes.py
+++ cans/cans/class_based/test_classes.py
@@ -5,31 +5,40 @@ from fitter import Fitter
 from model import CompModel, IndeModel
 
 
-
 comp_model = CompModel()
 inde_model = IndeModel()
 
-obs_times = np.linspace(0, 15, 16)
+obs_times = np.linspace(0, 15, 21)
+rows = 2
+cols = 2
 
-a_plate = Plate(2, 2)
+a_plate = Plate(rows, cols)
 a_plate.times = obs_times
+# Simulate some amounts for the plate
 a_plate.sim_params = comp_model.gen_params(a_plate, mean=1.0, var=1.0)
 a_plate.sim_amounts = comp_model.solve(a_plate, a_plate.sim_params)
-print(a_plate.sim_amounts)
-print("")
-#print([ data in a_plate.sim_amounts.flatten()]
-
 # Now add these amounts to Cultures on the plate and fit independent
 # model and save estimates.
-cells = []
-nuts = []
+a_plate.add_cultures_sim_data()
+print(a_plate.sim_amounts)
+print(a_plate.cultures[0].c_meas)
 
-culture_data = []
+# Create a Fitter instance which will fit the independent model.
+inde_fitter = Fitter(inde_model)
 
-for i in range(a_plate.no_cultures):
-    cells.append(a_plate.sim_amounts[:, i*2])
-    nuts.append(a_plate.sim_amounts[:, i*2+1])
-    culture_data.append(a_plate.sim_amounts[:, i*no_species:(i+1)*no_species])
-print(cells[0])
-print(nuts[0])
-print(culture_data[0])
+for culture in a_plate.cultures:
+    culture.inde_est = inde_fitter.fit_model(culture)
+
+
+# No take average of cells where cells is not the initital guess.
+# Take average of nutrients where nutrients is not zero.
+# Take estimated r values as start for whole plate fitting.
+
+#init_guess =
+
+inde_ests = [culture.inde_est for culture in a_plate.cultures]
+for est in inde_ests:
+    print(est.x)
+    print("")
+
+print(a_plate.sim_params)
