fb3e5944cdae89bed6068ce5a6185d25f286deee
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-11 12:57:10 +0100

    Test comp fit

--- cans/cans/class_based/test_classes.py
+++ cans/cans/class_based/test_classes.py
@@ -7,27 +7,27 @@ from model import CompModel, IndeModel
 inde_model = IndeModel()
 comp_model = CompModel()
 
-plate1 = Plate(2, 2)
+plate1 = Plate(3, 3)
 plate1.times = np.linspace(0, 15, 21)    # Set obs times
 
-# Simulate some params and solve for amounts for the plate.
-plate1.sim_params = comp_model.gen_params(plate1, mean=1.0, var=1.0)
-plate1.sim_amounts = comp_model.solve(plate1, plate1.sim_params)
-plate1.c_meas = plate1.sim_amounts.flatten()[::comp_model.no_species]
-plate1.set_cultures()    # Set culture.c_meas and times
+custom_params = {'kn': 0.001}    # Using a list we could store kn for several plates
+plate1.set_sim_data(comp_model, custom_params=custom_params)
 
-est = plate1.est_from_cultures()
+culture_ests = plate1.est_from_cultures()
+plate1.inde_est = plate1.fit_model(inde_model, plate1.est_from_cultures())
 
-# Now add these amounts to Cultures on the plate and fit independent
-# model and return estimates
-plate1.inde_est = plate1.fit_model(inde_model,
-                                   plate1.est_from_cultures())
+# Fit comp model using full plate inde estimate. Need to add a kn
+inde_plate_est = plate1.inde_est.x
+init_guess = np.insert(inde_plate_est, 2, 0.0)    # issue with inserting. How to make general?
+print(init_guess)
+plate1.comp_est = plate1.fit_model(comp_model, init_guess)
 
-# Now use inde est for comp fit.
 
 print("sim_params/true")
 print(plate1.sim_params)
 print("Culture ests")
-print(est)
-print("Plate ests")
+print(culture_ests)
+print("Plate inde ests")
 print(plate1.inde_est.x)
+print("Plate comp ests")
+print(plate1.comp_est.x)
