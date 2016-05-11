ad7f16bec26384b10802d7e79149ed48fc99d915
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-11 11:46:08 +0100

    Test updated classes

--- cans/cans/class_based/test_classes.py
+++ cans/cans/class_based/test_classes.py
@@ -4,41 +4,30 @@ from plate import Plate
 from fitter import Fitter
 from model import CompModel, IndeModel
 
-
-comp_model = CompModel()
 inde_model = IndeModel()
+comp_model = CompModel()
 
-obs_times = np.linspace(0, 15, 21)
-rows = 2
-cols = 2
+plate1 = Plate(2, 2)
+plate1.times = np.linspace(0, 15, 21)    # Set obs times
 
-a_plate = Plate(rows, cols)
-a_plate.times = obs_times
-# Simulate some amounts for the plate
-a_plate.sim_params = comp_model.gen_params(a_plate, mean=1.0, var=1.0)
-a_plate.sim_amounts = comp_model.solve(a_plate, a_plate.sim_params)
+# Simulate some params and solve for amounts for the plate.
+plate1.sim_params = comp_model.gen_params(plate1, mean=1.0, var=1.0)
+plate1.sim_amounts = comp_model.solve(plate1, plate1.sim_params)
+plate1.c_meas = plate1.sim_amounts.flatten()[::comp_model.no_species]
+plate1.set_cultures()    # Set culture.c_meas and times
+
+est = plate1.est_from_cultures()
+
 # Now add these amounts to Cultures on the plate and fit independent
-# model and save estimates.
-a_plate.add_cultures_sim_data()
-print(a_plate.sim_amounts)
-print(a_plate.cultures[0].c_meas)
+# model and return estimates
+plate1.inde_est = plate1.fit_model(inde_model,
+                                   plate1.est_from_cultures())
 
-# Create a Fitter instance which will fit the independent model.
-inde_fitter = Fitter(inde_model)
+# Now use inde est for comp fit.
 
-for culture in a_plate.cultures:
-    culture.inde_est = inde_fitter.fit_model(culture)
-
-
-# No take average of cells where cells is not the initital guess.
-# Take average of nutrients where nutrients is not zero.
-# Take estimated r values as start for whole plate fitting.
-
-#init_guess =
-
-inde_ests = [culture.inde_est for culture in a_plate.cultures]
-for est in inde_ests:
-    print(est.x)
-    print("")
-
-print(a_plate.sim_params)
+print("sim_params/true")
+print(plate1.sim_params)
+print("Culture ests")
+print(est)
+print("Plate ests")
+print(plate1.inde_est.x)
