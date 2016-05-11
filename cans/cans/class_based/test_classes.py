a45ec2c8028b2fea5827fa20470b48c5d43c6079
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-11 15:46:06 +0100

    Small structural changes

--- cans/cans/class_based/test_classes.py
+++ cans/cans/class_based/test_classes.py
@@ -1,32 +1,39 @@
 import numpy as np
 
+
 from plate import Plate
 from fitter import Fitter
 from model import CompModel, IndeModel
 
+
+# Instantiate both model types
 inde_model = IndeModel()
 comp_model = CompModel()
 
+# Define plate
 plate1 = Plate(3, 3)
 plate1.times = np.linspace(0, 15, 21)    # Set obs times
 
-custom_params = {'kn': 0.001}    # Using a list we could store kn for several plates
-plate1.set_sim_data(comp_model, custom_params=custom_params)
+# Give the plate some data
+kns = [{'kn': 0.01}]    # Could use a list of dicts to store several kns
+plate1.set_sim_data(comp_model, r_mean=1.0, r_var=1.0, custom_params=kns[0])
 
+
+
+# Make culture estimates then make full plate independent estimate.
 culture_ests = plate1.est_from_cultures()
-plate1.inde_est = plate1.fit_model(inde_model, plate1.est_from_cultures())
+plate1.inde_est = plate1.fit_model(inde_model, culture_ests)
 
-# Fit comp model using full plate inde estimate. Need to add a kn
-inde_plate_est = plate1.inde_est.x
-init_guess = np.insert(inde_plate_est, 2, 0.0)    # issue with inserting. How to make general?
-print(init_guess)
+# Add kn to inde_estimates then make a competition fit
+init_guess = np.insert(plate1.inde_est.x, comp_model.r_index, 0.0)
 plate1.comp_est = plate1.fit_model(comp_model, init_guess)
 
 
+print(init_guess)
 print("sim_params/true")
 print(plate1.sim_params)
 print("Culture ests")
-print(culture_ests)
+print(plate._get_avg_culture_ests())
 print("Plate inde ests")
 print(plate1.inde_est.x)
 print("Plate comp ests")
