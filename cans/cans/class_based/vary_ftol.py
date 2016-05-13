1ab962921fc3b1ee6db2dcfe4875ecc7a5b8ae61
Daniel Boocock <daniel.boocock@protonmail.ch>
2016-05-13 16:55:42 +0100

    Not testing empties anymore

--- cans/cans/class_based/vary_ftol.py
+++ cans/cans/class_based/vary_ftol.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 from plate import Plate
 from model import CompModel, IndeModel
@@ -20,40 +20,47 @@ kns = [{'kn': 0.1}]    # Could use a list of dicts to store several
 
 # Want a plate with an empty site. May have to give empties in
 # instantiation.
-emp_plate = Plate(3, 3)
-emp_plate.empties = (0, 2, 5)
+emp_plate = Plate(1, 1)
+#emp_plate.empties = (0, 2, 5)
 emp_plate.times = times
 emp_plate.set_sim_data(comp_model, r_mean=2.0, r_var=2.0, custom_params=kns[0])
 
 culture_ests = emp_plate.est_from_cultures()
 emp_plate.inde_est = emp_plate.fit_model(inde_model, culture_ests)
+
+emp_plate.inde_est.hello = "hello"
+print(emp_plate.inde_est.hello)
+print(emp_plate.inde_est.keys())
+print(emp_plate.inde_est["hello"])
+print(emp_plate.inde_est.fit_time)
+assert False
+
 # Add kn=0.0 to inde_est to form an initial guess. init_guess0 is used
 # so that if we loop the comp_fitter and update the init_guess every
 # time we still can print it at the bottom.
 init_guess0 = np.insert(emp_plate.inde_est.x, comp_model.no_species, 0.0)
-emp_plate.comp_est = emp_plate.fit_model(comp_model, init_guess0)
+init_guess = init_guess0
+timings = []
 
-inde_plotter = Plotter(inde_model)
-inde_plotter.plot_estimates(emp_plate, emp_plate.inde_est.x, sim=True)
-
-comp_plotter = Plotter(comp_model)    # No different to inde_plotter in this case.
-comp_plotter.plot_estimates(emp_plate, emp_plate.comp_est.x, sim=True)
-comp_plotter.plot_estimates(emp_plate, emp_plate.comp_est.x, sim=False)
+factrs = reversed([10**i for i in range(15)])
 
 
 
-print(emp_plate.sim_params)
-print(culture_ests)
-print(emp_plate.inde_est)
-print(emp_plate.comp_est)
-print(emp_plate.sim_amounts)
+for factr in factrs:
+    fit_options = {
+        'ftol': factr*np.finfo(float).eps
+    }
 
-# # Make culture estimates then make full plate independent estimate.
-# culture_ests = plate1.est_from_cultures()
-# plate1.inde_est = plate1.fit_model(inde_model, culture_ests)
+    t0 = time.time()
+    emp_plate.comp_est = emp_plate.fit_model(comp_model, init_guess,
+                                             custom_options=fit_options)
+    t1 = time.time()
+    timings.append(t1-t0)
 
-# # Add kn to inde_estimates then make a competition fit
-# init_guess0 = np.insert(plate1.inde_est.x, comp_model.no_species, 0.0)
-# init_guess = init_guess0
-# fun = np.inf
-# success = False
+    init_guess = emp_plate.comp_est.x
+
+    comp_plotter = Plotter(comp_model)
+    comp_plotter.plot_estimates(emp_plate, emp_plate.comp_est.x,
+                                title="factr = 10e"+str(np.log(factr)/np.log(10)), sim=True)
+
+print(timings)
