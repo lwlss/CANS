f093b033aa6f79e141ecfe7e42dc37291a4c3087
Dan Boo <daniel.boocock@protonmail.ch>
2016-05-16 18:43:45 +0100

    functions for getting zones from a plate

--- cans/cans2/cans_funcs.py
+++ cans/cans2/cans_funcs.py
@@ -1,7 +1,7 @@
 import numpy as np
 import copy
 import random
-
+import json
 
 
 def mad(a, b):
@@ -32,10 +32,55 @@ def dict_to_json(dct):
     return dct
 
 
-def get_zone(array, coords, rows, cols):
+def _get_zone(array, coords, rows, cols):
     """Return a zone of an array."""
     zone = array[coords[0]:coords[0]+rows, coords[1]:coords[1]+cols]
     return zone
+
+
+def plate_zone(resim=True):
+    """Return a plate from a zone of a larger plate.
+
+    If resim == True, resimulate from the underlying
+    parameters. Otherwise, return the c_measures from the larger
+    plate.
+
+    """
+    pass
+
+
+def get_zone_params(plate_file, coords, rows, cols):
+    """Return params for a zone of a plate saved as json.
+
+    Returns plate level parameters and in a flattened list.
+
+    """
+    # Read in the full plate.
+    with open(plate_file, 'r') as f:
+        plate_data = json.load(f)
+
+    plate_rows = plate_data['rows']
+    plate_cols = plate_data['cols']
+    times = plate_data['times']
+    r_index = len(plate_data['model_params']) - 1
+    plate_params = plate_data['sim_params']
+    plate_rs = plate_params[r_index:]
+
+    assert coords[0] + rows <= plate_rows
+    assert coords[1] + cols <= plate_cols
+    assert len(plate_rs) == plate_rows*plate_cols
+
+    # Convert the plate r parameters to an array.
+    plate_array = np.array(plate_rs)
+    plate_array.shape = (plate_rows, plate_cols)
+    for row in range(plate_rows):
+        assert all(plate_array[row, :] ==
+                   plate_rs[row*plate_cols:(row+1)*plate_cols])
+
+    # Now slice the plate array to get the required zone.
+    zone = _get_zone(plate_array, coords, rows, cols)
+    params = plate_params[:r_index] + zone.flatten().tolist()
+    return params
 
 
 def add_noise(data, sigma=0.02):
