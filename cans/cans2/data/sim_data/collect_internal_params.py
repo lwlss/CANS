455fb690df95ca5ad4594af2844a222246ba13e7
Dan Boo <daniel.boocock@protonmail.ch>
2016-05-16 16:31:26 +0100

    get_zone_params function

--- cans/cans2/data/sim_data/collect_internal_params.py
+++ cans/cans2/data/sim_data/collect_internal_params.py
@@ -6,7 +6,7 @@ import json
 from cans2.cans_funcs import get_zone
 
 
-def zone_plate(resim=True):
+def plate_zone(resim=True):
     """Return a plate from a zone of a larger plate.
 
     If resim == True, resimulate from the underlying
@@ -16,50 +16,49 @@ def zone_plate(resim=True):
     """
     pass
 
-def zone_params(plate_file, coord, rows, cols):
-    """Return params from a zone of a larger plate."""
+
+def get_zone_params(plate_file, coords, rows, cols):
+    """Return params for a zone of a plate saved as json.
+
+    Returns plate level parameters and in a flattened list.
+
+    """
     # Read in the full plate.
     with open(plate_file, 'r') as f:
         plate_data = json.load(f)
 
+    plate_rows = plate_data['rows']
+    plate_cols = plate_data['cols']
+    times = plate_data['times']
+    r_index = len(plate_data['model_params']) - 1
+    plate_params = plate_data['sim_params']
+    plate_rs = plate_params[r_index:]
 
-assert coords[0] + rows <= big_rows
-assert coords[1] + cols <= big_cols
+    assert coords[0] + rows <= plate_rows
+    assert coords[1] + cols <= plate_cols
+    assert len(plate_rs) == plate_rows*plate_cols
 
-# Read in the full plate.
-with open(plate_file, 'r') as f:
-    plate_data = json.load(f)
+    # Convert the plate r parameters to an array.
+    plate_array = np.array(plate_rs)
+    plate_array.shape = (plate_rows, plate_cols)
+    for row in range(plate_rows):
+        assert all(plate_array[row, :] ==
+                   plate_rs[row*plate_cols:(row+1)*plate_cols])
 
-times = plate_params['times']
-plate_params = plate_data['sim_params']
-plate_rs = plate_params[3:]
-assert len(plate_rs) == big_rows*big_cols
-
-# Convert the plate r parameters to an array.
-plate_array = np.array(plate_rs)
-plate_array.shape = (big_rows, big_cols)
-for row in range(big_rows):
-    assert all(plate_array[row, :] == plate_rs[row*big_cols:(row+1)*big_cols])
-
-# Now slice the plate array to get the required zone.
-zone = get_zone(plate_array, coords, rows, cols)
-params = plate_params[:3] + zone.flatten().tolist()
+    # Now slice the plate array to get the required zone.
+    zone = get_zone(plate_array, coords, rows, cols)
+    params = plate_params[:r_index] + zone.flatten().tolist()
+    return params
 
 
 if __name__ == '__main__':
     # Save a 5x5 and 2x1 plate to file.
-    from cans2.plate import Plate
 
-    zone1 = plate(rows, cols)
+    plate_file = '16x24_comp_model/mean_5_var_3.json'
 
-    # Rows and cols of first culture in sought zone (starts at (0, 0)).
-    coords = (6, 11)
-    # Number of rows and colums for internal zone.
-    rows = 5
-    cols = 5
+    zone_5x5 = get_zone_params(plate_file, coords=(6, 11),
+                               rows=5, cols=5)
+    # zone_2x1 = get_zone_params(plate_file, coords=(6, 11),
+    #                            rows=2, cols=1)
 
-
-    # Rows and cols of full plate.
-    big_rows = 16
-    big_cols = 24
-    plate_file = '{0}x{1}_comp_model/mean_5_var_3.json'.format(big_rows, big_cols)
+    print(zone_5x5)
