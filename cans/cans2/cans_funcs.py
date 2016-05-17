338f424d9d3147ebd7317d3e063f0d7893e94c01
Dan Boo <daniel.boocock@protonmail.ch>
2016-05-17 11:40:56 +0100

    Add general calc_devs function

--- cans/cans2/cans_funcs.py
+++ cans/cans2/cans_funcs.py
@@ -9,6 +9,23 @@ def mad(a, b):
     return np.mean(np.abs(a - b))
 
 
+def calc_devs(true_params, r_index, *ests):
+    """Return deviations of estimated parameters from true.
+
+    For r return mean absolute deviation. Requires index of first
+    r_value. Lists should be of equal length.
+    """
+    true_plate_lvl = true_params[:r_index]
+    true_rs = true_params[r_index:]
+    devs = []
+    for est in ests:
+        assert len(true_params) == len(est)
+        dev = np.abs(true_plate_lvl - est[:r_index])
+        dev = np.append(dev, mad(true_rs, est[r_index:]))
+        devs.append(dev)
+    return devs
+
+
 def gauss_list(n, mean=1.0, var=1.0, negs=False):
     """Return a list of n random gaussian distributed numbers.
 
