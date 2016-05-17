f6021c079d0ef1bda946dfc8ec049241c0344f1d
Dan Boo <daniel.boocock@protonmail.ch>
2016-05-17 14:57:18 +0100

    Use zone for init_guess

--- cans/cans2/data/fit_comp_sim.py
+++ cans/cans2/data/fit_comp_sim.py
@@ -3,25 +3,26 @@ import json
 import datetime
 
 
+from cans2.zoning import get_zone_r_guess
 from cans2.plate import Plate
 from cans2.model import CompModel
 #from cans2.plotter import Plotter
 from cans2.funcs import calc_devs, dict_to_json
 
 
-def save_as_json(true_plate, est_plate, est, factr, init_guess, model,
-                 plate_file, init_guess_file, outfile):
+def save_as_json(true_plate, est_plate, est, factr, init_guess,
+                 plate_file, guess_file, out_file):
 
-    assert all(est_plate.sim_params == est)
-
-    # Calculate parameter deviations
-    assert all(true_data['params'] == model['params'])
-    param_devs = calc_devs(true_plate.sim_params, model.r_index,
-                           est_plate.sim_params)
+    assert np.array_equal(est_plate.sim_params, est.x)
 
     # Read true data from file.
     with open(plate_file, 'r') as f:
         true_data = json.load(f)
+
+    # Calculate parameter deviations
+    assert np.array_equal(true_data['params'], est.model.params)
+    param_devs = calc_devs(true_plate.sim_params, est.model.r_index,
+                           est_plate.sim_params)[0]
 
     data = {
         'true_params': true_plate.sim_params,
@@ -37,23 +38,23 @@ def save_as_json(true_plate, est_plate, est, factr, init_guess, model,
         'sim_model_species': true_data['model_species'],
         'sim_model_params': true_data['model_params'],
 
-        'fit_model': model.name,
-        'fit_model_species': model.species,
-        'fit_model_params': model.params,
+        'fit_model': est.model.name,
+        'fit_model_species': est.model.species,
+        'fit_model_params': est.model.params,
 
         'plate_file': plate_file,
-        'init_guess_file': init_guess_file,
+        'init_guess_file': guess_file,
         'init_guess': init_guess,
         'est_params': est_plate.sim_params,
         'param_devs': param_devs,
         'est_amounts': est_plate.sim_amounts,
         'est_c_meas': est_plate.c_meas,
 
-        'fit_bounds': est.bounds,
         'fit_options': est.fit_options,    # Dict already jsoned.
         'ftol': est.fit_options['ftol'],
         'factr': factr,
         'machine_eps': np.finfo(float).eps,
+        'fit_bounds': est.bounds,
         'fit_method': est.method,
 
         'obj_fun_val': est.fun,
@@ -66,51 +67,60 @@ def save_as_json(true_plate, est_plate, est, factr, init_guess, model,
         'nfev': est.nfev,
         'tot_nfev': est.tot_nfev,
         'nit': est.nit,
-        'tot_nit': est.tot_init,
+        'tot_nit': est.tot_nit,
 
         'date': datetime.date.today(),
-        'description': '',
+        'description': ('If smaller than a 16x24 plate init_guesses are'
+                        'the first no_cultures r values in the file.'),
     }
     data = dict_to_json(data)
 
-    # with open(outfile, 'w') as f:
-    #     json.dump(data, f, sort_keys=True, indent=4)
+    with open(out_file, 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=4)
 
 
 
 # Parse in a number for an initial guess.
 guess_no = 0
-
-
 rows = 2
 cols = 1
 model = CompModel()
+out_dir = "{0}x{1}_comp_model/init_guess_{2}/".format(rows, cols, guess_no)
+true_file = "sim_data/16x24_comp_model/2x1_zone.json".format(rows, cols)
 
 # Read in true data
-true_file = "sim_data/16x24_comp_model/2x1_zone.json"
 with open(true_file, 'r') as f:
     true_data = json.load(f)
+
+# Find coords on full plate if exist
+if 'coords_on_parent' in true_data.keys():
+    coords = true_data['coords_on_parent']
+else:
+    coords = []
 
 true_plate = Plate(rows, cols)
 true_plate.times = true_data['times']
 true_plate.sim_params = true_data['sim_params']
 true_plate.set_sim_data(model)
-assert all(true_plate.c_meas == true_data['c_meas'])
+assert np.equal(true_plate.c_meas, true_data['c_meas'])
 
 
-# MAKE GUESS. Just use a slice of 16x24 random rs for smaller
-# arrays. Could also have done for sim params but not if we have also
-# simed amounts.
-guess_file = "init_guess/16x24_rs_mean_5_var_3/16x24_rs_{}.json"
+# MAKE GUESS
+guess_file = "init_guess/16x24_rs_mean_5_var_3/16x24_rs_{}.json"   # zones
 guess_file = guess_file.format(guess_no)
-
 # C_0, N_0, kn
 plate_lvl_guess = [0.00001, 1.2, 0.0]
-
 # Read in an initial guess for rs
 with open(guess_file, 'r') as f:
     guess_data = json.load(f)
-r_guess = guess_data['rand_rs'][:rows*cols]
+r_guess = guess_data['rand_rs']
+# If coords are given for a zone of a larger plate take the initial
+# guess from that zone. This may not make much of a difference but is
+# more consistant.
+if len(r_guess) < rows*cols and coords:
+    r_guess =  get_zone_r_guess(r_guess, coords, rows, cols)
+elif len(r_guess) < rows*cols and not coords:
+    r_guess = r_guess[:rows*cols]
 assert len(r_guess) == rows*cols
 init_guess = plate_lvl_guess + r_guess
 assert len(init_guess) == len(true_data['sim_params'])
@@ -129,6 +139,8 @@ this_plate.times = true_plate.times
 # Sim params is actually the init_guess
 this_plate.sim_params = init_guess
 this_plate.set_sim_data(model)
+
+
 
 for factr in factrs:
     fit_options = {
@@ -153,7 +165,10 @@ for factr in factrs:
     est_plate.sim_params = this_plate.comp_est.x
     est_plate.set_sim_data(model)
 
-    save_as_json(true_plate, est_plate, est, factr,)
+    save_as_json(true_plate, est_plate, this_plate.comp_est, factr,
+                 init_guess, true_file, guess_file, out_file)
+
+    save_fit(est_plate, this_plate.comp_est, factr, out_file)
 
     this_plate = est_plate
 
