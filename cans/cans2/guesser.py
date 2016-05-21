98fbab8e816e1eb30fb77980dd2881a2a3731cec
Dan Boo <daniel.boocock@protonmail.ch>
2016-05-21 23:26:23 +0100

    Plot grad with r_mean r_var

--- cans/cans2/guesser.py
+++ cans/cans2/guesser.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
             plate1.times = times
             true_params = {'N_0': 0.1, 'kn': kn}
             true_params['C_0'] = true_params['N_0']/10000
-            plate1.set_sim_data(model, 50.0, 25.0, true_params)
+            plate1.set_sim_data(model, r_mean, r_var, true_params)
 
             # comp_plotter.plot_est(plate1, plate1.sim_params)
 
@@ -94,30 +94,53 @@ if __name__ == '__main__':
 
     kns = [x/100 for x in range(31)]
 
-    m, c = fit_C_f_var_vs_kn(kns, times, comp_model, comp_guesser,
-                             16, 24, r_mean=r_mean, r_var=r_var)
-    print(m, c)
-
+    # Study r_mean and r_var effect on gradient
     r_means = [20.0, 40.0, 60.0, 80.0, 100.0]
-
     r_vars = [x*10.0 for x in range(8)]
 
-
     # vary r_mean and plot against gradient in C_f_var vs kn.
-    m_c = []
+    r_mean_m_c = []
     for r_mean in r_means:
+        r_mean_m_c.append(list(fit_C_f_var_vs_kn(kns, times, comp_model,
+                                          comp_guesser, rows=rows,
+                                          cols=cols, r_mean=r_mean)))
+    r_mean_m_c = np.array(r_mean_m_c)
+
+
+
+    # For fixed r_mean and r_var, the variance in final cell
+    # measurement follows a linear relationship. If we can find good
+    # guesses for r then we can guess kn (and possibly constrain it).
 
 
     # vary r_var and plot against gradient in C_f_var vs kn.
+    r_var_m_c = []
+    for r_var in r_vars:
+        r_var_m_c.append(list(fit_C_f_var_vs_kn(kns, times, comp_model,
+                                          comp_guesser, r_var=r_var)))
+    r_var_m_c = np.array(r_var_m_c)
+
+    print(r_mean_m_c)
+    print(r_var_m_c)
+    # plot m against r_mean
+    plt.plot(r_means, r_mean_m_c[:, 0])
+    plt.show()
+    plt.close()
+
+    # plot m against r_var
+    plt.plot(r_vars, r_var_m_c[:, 0])
+    plt.show()
+    plt.close()
 
 
-        # plt.plot(kns, C_f_vars, label="Final Cells var", marker="x", linestyle="None")
-        # plt.legend(loc='best')
-        # plt.show()
-        # plt.close()
 
-#     var_res = np.vstack([res[:, 0], np.ones(len(res[:, 0]))]).T
-#     print(var_res)
+    # plt.plot(kns, C_f_vars, label="Final Cells var", marker="x", linestyle="None")
+    # plt.legend(loc='best')
+    # plt.show()
+    # plt.close()
+
+    # var_res = np.vstack([res[:, 0], np.ones(len(res[:, 0]))]).T
+    # print(var_res)
 
 
-# kn ~ cf_var - r_var?
+    # kn ~ cf_var - r_var?
