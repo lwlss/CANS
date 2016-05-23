182548b95515acb126e6582f3594c736565a4fa9
Dan Boo <daniel.boocock@protonmail.ch>
2016-05-23 13:02:32 +0100

    Investigate bound on k for different r dists

--- cans/cans2/test_guess_model.py
+++ cans/cans2/test_guess_model.py
@@ -3,6 +3,8 @@ from cans2.model import CompModel, GuessModel
 from cans2.guesser import Guesser
 from cans2.plate import Plate
 from cans2.plotter import Plotter
+from cans2.cans_funcs import gauss_list
+import matplotlib.pyplot as plt
 
 comp_model = CompModel()
 guess_model = GuessModel()
@@ -10,7 +12,7 @@ comp_guesser = Guesser(comp_model)
 
 # generate a 2x2 plate
 
-plate = Plate(2, 2)
+plate = Plate(16, 24)
 plate.times = np.linspace(0, 5, 11)
 true_params = {'N_0': 0.1, 'kn': 0.2}
 true_params['C_0'] = true_params['N_0']/10000
@@ -27,9 +29,21 @@ for culture in plate.cultures:
                                           custom_options={'disp': False})
 
 
-print(plate.sim_params[3:])
-for culture in plate.cultures:
-    print(culture.guess_est.x[-1])
+# Find mean and variance. Or fit the comp model with these params to
+# find kn.
+rs = [culture.guess_est.x[-1] for culture in plate.cultures]
+rs.sort()
+print(rs)
+rs_mean = np.mean(rs[:-5])    # you get some outliers
+rs_var = np.var(rs[:-10])
+print(rs_mean, np.sqrt(rs_var))
+
+plt.hist(rs)
+
+# why are the outliers so high? Is k at the bounds?
+guesses = [culture.guess_est.x[-2:] for culture in plate.cultures]
+guesses.sort(key = lambda x: x[-1])
+print(np.array(guesses))
 
 # So try fixing N_0 and C_0
 guess_plotter = Plotter(guess_model)
