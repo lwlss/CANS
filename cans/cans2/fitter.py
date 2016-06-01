6324a441d6d77d96816e43ec44d52099f9d783d1
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-01 11:01:23 +0100

    Make long line two lines

--- cans/cans2/fitter.py
+++ cans/cans2/fitter.py
@@ -29,10 +29,10 @@ class Fitter:
         # params = copy.deepcopy(params)
         params[0] = params[0]/10000
         # Find amounts by solving the model using the estimated parameters.
-        c_est = np.split(self.model.solve(plate, params),
-                         self.model.no_species, axis=1)[0].flatten()
+        amount_est = self.model.solve(plate, params)
         # Mutable so must scale C_0 back
         params[0] = params[0]*10000
+        c_est = np.split(amount_est, self.model.no_species, axis=1)[0].flatten()
         # Zeros appear in here for empty plates but this shouldn't
         # have any effect.
         err = np.sqrt(np.sum((plate.c_meas - c_est)**2))
