25e77dc5d865b1e7a86b732293409d7f3612a670
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-02 21:44:31 +0100

    Ready to run 1210 5x5 fits

--- cans/cans2/guess_within_bounds/run_fits.sh
+++ cans/cans2/guess_within_bounds/run_fits.sh
@@ -2,7 +2,7 @@
 
 # Run multiple scripts in background and pass an arguement to index a
 # set inital guesses of kn and C_0. Run using bash run_fits.sh &.
-for vals in {1..100}
+for i in {0..1209}   # 1209
 do
-    python fit_real_zone.py $vals &
+    python fit_real_zone.py $i &
 done
