37127fd9306a6fbc3c969ed3e99d175c63c51899
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-06 12:56:12 +0100

    Rerun missing data

---
+++ cans/cans2/guess_within_bounds/run_missing_fits.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run multiple scripts in background and pass an arguement to index a
+# set inital guesses of kn and C_0. Run using bash run_fits.sh &.
+for i in {0..1209}   # 1209
+do
+    python fit_real_zone.py $i 0 &
+done
