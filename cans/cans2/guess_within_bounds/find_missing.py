45e25558dcf776d909f0f21b3b0f02f6481f847c
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-06 12:10:38 +0100

    Change file grouping to nested list comprehension

--- cans/cans2/guess_within_bounds/find_missing.py
+++ cans/cans2/guess_within_bounds/find_missing.py
@@ -19,9 +19,8 @@ path = "results/p15_fits/plots/"
 all_files = [f for f in glob.glob(os.path.join(path, "*.pdf"))]
 
 # Group files according to coordinates of zone.
-files = []
-for coord in coords:
-    files.append([f for f in all_files if "coords_{0}_{1}".format(*coord) in f])
+files = [[f for f in all_files if "coords_{0}_{1}".format(*coord) in f]
+         for coord in coords]
 
 # Find missing arguments from filenames.
 present_args = [[f.split("_")[-1].split(".")[0] for f in fs] for fs in files]
