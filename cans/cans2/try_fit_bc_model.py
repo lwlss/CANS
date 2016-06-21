5764c127b2f3aac7fe13077c06d067d0244a8762
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-21 19:45:33 +0100

    Inspect rr.fitting

--- cans/cans2/try_fit_bc_model.py
+++ cans/cans2/try_fit_bc_model.py
@@ -8,22 +8,47 @@ from cans2.guesser import Guesser
 # First try solving and plotting ordinary CompModel
 comp_model = CompModel(rev_diff=False)
 
-plate1 = Plate(3, 3)
+plate1 = Plate(2, 2)
 plate1.times = np.linspace(0, 5, 11)
 
 true_params = {"N_0": 0.1, "kn": 1.0}
-true_params["C_0"] = true_params["N_0"]/100000
+true_params["C_0"] = true_params["N_0"]/10000
 
 # Also sets rr_model with the simulated params
 plate1.set_sim_data(comp_model, b_mean=50.0, b_var=15.0,
-                    custom_params=true_params)
+                    custom_params=true_params, noise=False)
 
 comp_plotter = Plotter(comp_model)
-comp_plotter.plot_est(plate1, plate1.sim_params,
-                      title="CompModel Simulation", sim=True)
+# comp_plotter.plot_est(plate1, plate1.sim_params,
+#                       title="CompModel Simulation", sim=True)
 
 comp_guesser = Guesser(comp_model)
+guess = comp_guesser.make_guess(plate1)
+param_guess = [guess["N_0"]*1e-5, guess["N_0"], 1.0]
+print("param_guess", param_guess)
+r_guesses = [45 for i in range(plate1.no_cultures)]
+param_guess = param_guess + r_guesses
 
+bounds = comp_guesser.get_bounds(plate1, guess)
+bounds[0] = (guess["N_0"]*1.0e-10, guess["N_0"]/100)
+bounds[2] = (0.0, 11.00)
+# print(bounds)
+# Now test fitting of CompModel
+# print(plate1.sim_params)
+# print(len(plate1.c_meas))
+# print(plate1.c_meas)
+# plate1.comp_est = plate1.fit_model(comp_model,
+#                                    param_guess=param_guess,
+#                                    minimizer_opts={"disp": True},
+#                                    bounds=bounds, rr=False, sel=False)
+
+plate1.comp_est = plate1.fit_model(comp_model,
+                                   param_guess=param_guess,
+                                   minimizer_opts={"disp": True},
+                                   bounds=bounds, rr=True, sel=False)
+
+
+assert False
 # Then try CompModelBC
 
 comp_mod_bc = CompModelBC()
