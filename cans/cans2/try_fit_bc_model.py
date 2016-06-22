6b98f0284cb2247bc7a5d4835c082b5c8cfdb9ad
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-22 21:52:23 +0100

    Success! fit boundary condition model

--- cans/cans2/try_fit_bc_model.py
+++ cans/cans2/try_fit_bc_model.py
@@ -37,72 +37,63 @@ param_guess = param_guess + r_guesses
 bounds = comp_guesser.get_bounds(plate1, guess)
 bounds[0] = (guess["N_0"]*1.0e-10, guess["N_0"]/100)
 bounds[2] = (0.0, 11.00)
-assert len(bounds) == len(param_guess)
-# print(bounds)
+
 # Now test fitting of CompModel
-# print(plate1.sim_params)
-# print(len(plate1.c_meas))
-# print(plate1.c_meas)
+
+# odeint
 # plate1.comp_est = plate1.fit_model(comp_model,
 #                                    param_guess=param_guess,
 #                                    minimizer_opts={"disp": True},
 #                                    bounds=bounds, rr=False, sel=False)
-
-
-
-
-# Try plotting the est using both the cans and rr solver. I.e. do same
-# params give same timecourses?
-
-plotter = Plotter(comp_model)
-
-# plate1.comp_est2 = plate1.fit_model(comp_model,
-#                                     param_guess=plate1.comp_est.x,
-#                                     minimizer_opts={"disp": True},
-#                                     bounds=bounds, rr=False, sel=False)
-
-
-print("")
-print("fitting")
-plate1.rr.reset()
-rr_Fitter = Fitter(comp_model)
-plate1.comp_est_rr = rr_Fitter.fit_model(plate1,
-                                         param_guess=param_guess,
-                                         minimizer_opts={"disp": True},
-                                         bounds=bounds, rr=True, sel=False)
-print("true", plate1.sim_params)
+# roadrunner
+# plate1.comp_est_rr = plate1.fit_model(comp_model,
+#                                       param_guess=param_guess,
+#                                       minimizer_opts={"disp": True},
+#                                       bounds=bounds, rr=True,
+#                                       sel=False)
+# print("true", plate1.sim_params)
 # print("odeint", plate1.comp_est.x)
-print("rr", plate1.comp_est_rr.x)
-
+# print("rr", plate1.comp_est_rr.x)
+# plotter = Plotter(comp_model)
 # plotter.plot_est(plate1, plate1.comp_est.x, title="Cans solver")
-plotter.plot_est_rr(plate1, plate1.comp_est_rr.x, title="RR solver")
+# plotter.plot_est_rr(plate1, plate1.comp_est_rr.x, title="RR solver")
 
-
-# Try solving and plotting the best estimate with both solvers and see
-# what happens.
-
-assert False
 # Then try CompModelBC
 
 comp_mod_bc = CompModelBC()
 comp_plotter_bc = Plotter(comp_mod_bc)
 
-plate_bc = Plate(6, 6)
+plate_bc = Plate(5, 5)
 plate_bc.times = np.linspace(0, 5, 11)
 
-
 true_params_bc = {"N_0": 0.1, "NE_0": 0.15, "kn": 1.0}
-true_params_bc["C_0"] = true_params_bc["N_0"]/1000000
+true_params_bc["C_0"] = true_params_bc["N_0"]/10000
+
+param_guess = [0.08/10000, 0.08, 0.13, 1.0]
+r_guesses = [45 for i in range(plate_bc.no_cultures)]
+param_guess = param_guess + r_guesses
+bounds_bc = bounds[:3]
+bounds_bc[1] = (0.08, 0.12)
+bounds_bc = list(bounds_bc)
+bounds_bc.insert(2, (0.13, 0.17))
+r_bounds_bc = [(0.0, None) for i in range(plate_bc.no_cultures)]
+bounds_bc = np.asarray(bounds_bc + r_bounds_bc)
+print(bounds_bc)
+print(len(param_guess))
+print(len(bounds_bc))
+
 # Also sets rr_model with the simulated params
 plate_bc.set_sim_data(comp_mod_bc, b_mean=50.0, b_var=15.0,
                       custom_params=true_params_bc)
 
-comp_plotter_bc.plot_est(plate_bc, plate_bc.sim_params, sim=True)
+comp_plotter_bc.plot_est_rr(plate_bc, plate_bc.sim_params, sim=True)
 
-# plate_bc.est = plate_bc.fit_model(comp_mod_bc, minimizer_opts={"disp": True},
-#                               rr=True, sel=False)
-# print(plate_bc.est.x)
-# print(plate_bc.sim_params)
+plate_bc.est = plate_bc.fit_model(comp_mod_bc,
+                                  param_guess=param_guess,
+                                  bounds=bounds_bc,
+                                  minimizer_opts={"disp": True},
+                                  rr=True, sel=False)
+print(plate_bc.est.x)
+print(plate_bc.sim_params)
 
-
-# comp_plotter.plot_est(plate_bc, plate_bc.est.x, sim=True)
+comp_plotter_bc.plot_est_rr(plate_bc, plate_bc.est.x, sim=True)
