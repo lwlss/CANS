aa6e1d10023e8d81192d2be91c13b452caf69845
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-26 21:07:03 +0100

    Finished imaginary neighbours quick fitting.

--- cans/cans2/try_imag_neigh_guesser.py
+++ cans/cans2/try_imag_neigh_guesser.py
@@ -2,19 +2,19 @@ import numpy as np
 
 
 from cans2.plate import Plate
-from cans2.model import CompModel, CompModelBC, IndeModel
+from cans2.model import CompModel, CompModelBC, ImagNeighModel
 from cans2.guesser import Guesser, sim_a_plate, fit_imag_neigh
 from cans2.cans_funcs import gauss_list
 from cans2.plotter import Plotter
 
 
 model = CompModelBC()
-rows = 3
-cols = 3
+rows = 8
+cols = 8
 times = np.linspace(0, 5, 11)
 true_params = {"N_0": 0.1, "NE_0": 0.15, "kn": 1.0}
 true_params["C_0"] = true_params["N_0"]/10000
-bs = gauss_list(rows*cols, mean=50.0, var=15.0, negs=False)
+bs = gauss_list(rows*cols, mean=50.0, var=30.0, negs=False)
 # Make a np.array of true params.
 true_params = np.array([true_params[p] for p in model.params[:model.b_index]]
                        + list(bs))
@@ -39,21 +39,20 @@ quick_fit_kwargs = {
     # and the C_ratio argument. ['kn1', 'kn2', 'b-', 'b+', 'b'] The
     # final parameter is the guess of the CompModel parameter b. Other
     # parameters are uniquie to the imaginary neighbour model.
-    "imag_neigh_params": np.array([1.0, 1.0, 0.0, 60.0, 45.0]),
+    "imag_neigh_params": np.array([1.0, 1.0, 0.0, 50.0, 45.0]),
     "no_neighs": None,    # Can specify or allow it to be calculated np.ceil(C_f/N_0)
-    "C_ratio": 1e-6,    # Guess of init_cells/final_cells.
+    "C_ratio": 1e-4,    # Guess of init_cells/final_cells.
     # "C_doubt": 1e2,
     # "N_doubt": 2.0,
 }
 
 quick_guess, quick_guesser = fit_imag_neigh(**quick_fit_kwargs)
 # Need to add in a kn guess.
-
-quick_guess = np.delete(quick_guess, model.params.index("kn"))
-
-inde_model = IndeModel()
-plotter = Plotter(model)
-plotter.plot_culture_fits(quick_guesser.plate, inde_model, sim=True)
+quick_guess[model.params.index("kn")] = true_params[model.params.index("kn")]
 
 print("true_parms", true_params)
 print("quick_guess", quick_guess)
+
+# Plot a rr model solution using the estimated b params and the true kn
+plotter = Plotter(model)
+plotter.plot_est_rr(quick_guesser.plate, quick_guess, sim=True)
