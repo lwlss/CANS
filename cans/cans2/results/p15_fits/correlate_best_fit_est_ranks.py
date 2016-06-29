907bf21fae846793727e21a90be5a0f7bde75ed4
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-29 16:01:34 +0100

    Use Spearman's rho for correlations.

--- cans/cans2/results/p15_fits/correlate_best_fit_est_ranks.py
+++ cans/cans2/results/p15_fits/correlate_best_fit_est_ranks.py
@@ -1,7 +1,9 @@
 import numpy as np
 import json
+import Bio.Cluster
 
 
+from cans2.model import CompModelBC
 from cans2.process import find_best_fits, test_bounds
 
 
@@ -20,4 +22,16 @@ guess_key = "guess_method_C_ratio_zero_kn"
 guess_method = [str(d[guess_key][0]) for d in data]
 print(guess_method)
 
+model = CompModelBC()
+
 # Make a matrix of correlations of rank orders.
+b_ests = np.array([d["comp_est"][model.b_index:] for d in data])
+
+
+
+
+def euclidean_distance(x, y):
+    return Bio.Cluster.distancematrix((x, y), dist="e")[1][0]
+
+distances = spearmans_rho(b_ests)
+print(distances)
