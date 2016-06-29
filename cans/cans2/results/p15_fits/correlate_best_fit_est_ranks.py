ad6c164ae8354864e9a10616e5b08e83613c31ce
Dan Boo <daniel.boocock@protonmail.ch>
2016-06-29 16:52:56 +0100

    Calculate mads and spearmans correlations

--- cans/cans2/results/p15_fits/correlate_best_fit_est_ranks.py
+++ cans/cans2/results/p15_fits/correlate_best_fit_est_ranks.py
@@ -1,13 +1,14 @@
 import numpy as np
 import json
 import Bio.Cluster
+import itertools
 
 
 from cans2.model import CompModelBC
-from cans2.process import find_best_fits, test_bounds
+from cans2.process import find_best_fits, test_bounds, spearmans_rho, mad_tril
 
 
-best_fits = np.array(find_best_fits("full_plate/*.json", 10, key="obj_fun"))
+best_fits = np.array(find_best_fits("full_plate/*.json", 5, key="obj_fun"))
 
 data = []
 for filename in best_fits[:, 0]:
@@ -27,11 +28,13 @@ model = CompModelBC()
 # Make a matrix of correlations of rank orders.
 b_ests = np.array([d["comp_est"][model.b_index:] for d in data])
 
-
-
-
-def euclidean_distance(x, y):
-    return Bio.Cluster.distancematrix((x, y), dist="e")[1][0]
-
-distances = spearmans_rho(b_ests)
+distances = (spearmans_rho(b_ests))
 print(distances)
+
+mads = mad_tril(b_ests)
+print(mads)
+
+
+
+# for x, y in np.nditer([b_ests, b_ests]):
+#     print(vmad(x, y))
