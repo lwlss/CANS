9c2515bb4e136b4a097cdddb2bcbfbfe7e89fa4b
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-05 18:00:49 +0100

    Also plot guess and write first 10 params to csv

--- cans/cans2/results/p15_fits/replot_fit.py
+++ cans/cans2/results/p15_fits/replot_fit.py
@@ -1,6 +1,7 @@
 """Script to replot fits in matplotlib window to save manually."""
 import numpy as np
 import json
+import csv
 
 
 from cans2.plotter import Plotter
@@ -12,15 +13,25 @@ from cans2.plate import Plate
 # plot options/appearance in the plotter.plot_est function in last
 # line. If sim=True, true nutrient amounts are also plotted (these are
 # not used in fitting).
-argv = 5
-b_guess = 35
+argv = 19
+b_guess = 45
 
-data_file = "full_plate/argv_{0}_b_guess_{1}.json"
+data_file = "full_plate/CompModelBC/argv_{0}_b_guess_{1}.json"
 data_file = data_file.format(argv, b_guess)
 
 # Read in data from json file
 with open(data_file, 'r') as f:
     fit_data = json.load(f)
+
+# r0 = ["argv_{0}_b_guess_{1}".format(argv, b_guess)]
+# r1 = ["est_params"] + fit_data["comp_est"][:10]
+# r2 = ["log_eq_guess"] + fit_data["init_guess"][:10]
+# rows = [r0, r1, r2]
+# outfile = "full_plate/replots/first_10_params.csv"
+# with open(outfile, 'ab') as f:
+#     writer = csv.writer(f, delimiter="\t")
+#     for r in rows:
+#         writer.writerow(r)
 
 plate = Plate(fit_data["rows"], fit_data["cols"])
 models = [CompModel(), CompModelBC()]
@@ -36,6 +47,10 @@ plate.set_rr_model(model, fit_data['comp_est'])
 
 plot_title = 'Competition Model BC fit to p15 (argv {0}; b_guess {1})'
 plot_title = plot_title.format(argv, b_guess)
-# markersize, markeredgewidth, linewidth
 plotter.plot_est_rr(plate, fit_data["comp_est"], title=plot_title,
-                    sim=False, legend=False)
+                    sim=False, legend=False, ms=10.0, mew=0.5, lw=2.5)
+
+plot_title = 'Competition Model BC init guess for p15 (argv {0}; b_guess {1})'
+plot_title = plot_title.format(argv, b_guess)
+plotter.plot_est_rr(plate, fit_data["init_guess"], title=plot_title,
+                    sim=False, legend=False, ms=10.0, mew=0.5, lw=2.5)
