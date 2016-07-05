8028d3a94ceafdedd932fd34147866bc57c80f8b
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-05 21:49:41 +0100

    Try wrapping to pickle SWIG objects.

--- cans/cans2/try_genetic.py
+++ cans/cans2/try_genetic.py
@@ -35,12 +35,73 @@ eval_kwargs = {
     "plate_lvl": data["sim_params"][:-no_cultures],
     "fitter": Fitter(),
     "model": CompModelBC().name,
+    "plate": Plate(**get_plate_kwargs(data)),
+#    "model": CompModelBC(),
 }
 
 args = {
     "gen_kwargs": gen_kwargs,
     "eval_kwargs": eval_kwargs,
 }
+pickleable(args)
 
-finlal_pop = mp_evolver(gen_random_uniform, evaluate_b_candidate, bounds, args,
-                        cpus=4, pop_size=100, max_evals=1000, mut_rate=0.25)
+# final_pop = mp_evolver(gen_random_uniform, evaluate_b_candidate, bounds, args,
+#                        cpus=4, pop_size=100, max_evals=1000, mut_rate=0.25)
+
+
+class PickleableSWIG(object):
+    # http://stackoverflow.com/a/9325185
+    def __setstate__(self, state):
+        print(state)
+        self.__init__(*state['args'])
+
+    def __getstate__(self):
+        return {'args': self.args}
+
+
+class PickleableCompModelBC(CompModelBC, PickleableSWIG):
+    # http://stackoverflow.com/a/9325185
+    def __init__(self, *args):
+        self.args = args
+        CompModelBC.__init__(self)
+
+
+class PickleablePlate(Plate, PickleableSWIG):
+    # http://stackoverflow.com/a/9325185
+    def __init__(self, *args):
+        self.args = args
+        Plate.__init__(self, **self.args[0])
+
+
+pickleable_model = PickleableCompModelBC()
+pickleable(pickleable_model)
+
+plate = PickleablePlate(get_plate_kwargs(data))    # Must send as an arg not **kwargs.
+plate.set_rr_model(pickleable_model, data["sim_params"])
+
+import pickle
+import roadrunner
+class PickleableRoadRunner(roadrunner.RoadRunner,  PickleableSWIG):
+    # http://stackoverflow.com/a/9325185
+    def __init__(self, *args):
+        self.args = args
+        roadrunner.RoadRunner.__init__(self)
+
+class PickleableRRModel(roadrunner.RoadRunner.,  PickleableSWIG):
+    # http://stackoverflow.com/a/9325185
+    def __init__(self, *args):
+        self.args = args
+        roadrunner.RoadRunner.__init__(self)
+
+rr = plate.rr
+pickleable_rr = PickleableRoadRunner()
+rr = pickle.loads(pickle.dumps(pickleable_rr))
+print(rr)
+# pickle.dumps(rr)
+plate.rr = rr
+sol = plate.rr_solve()
+print(sol)
+
+dump = pickle.dumps(plate)
+# print(dump)
+pickleable(plate)
