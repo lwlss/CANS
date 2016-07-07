c41208ef677591751ea0ed3d5d613dc452c24200
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-07 13:50:17 +0100

    Try serial bs evolution with ES

--- cans/cans2/try_genetic.py
+++ cans/cans2/try_genetic.py
@@ -1,54 +1,21 @@
 import time
 import json
 import numpy as np
-import roadrunner
 
 
-from cans2.genetic2 import get_plate_kwargs, mp_evolver, gen_random_uniform, evaluate_b_candidate
-from cans2.plate import Plate
+from cans2.cans_funcs import dict_to_numpy
 from cans2.model import CompModelBC
-from cans2.cans_funcs import dict_to_numpy, pickleable
-from cans2.fitter import Fitter
-from cans2.make_sbml import create_sbml
+from cans2.plate import Plate
+from cans2.plotter import Plotter
+from cans2.genetic2 import evolver, mp_evolver, gen_random_uniform, evaluate_b_candidate, evaluate_b_candidates
+from cans2.genetic_kwargs import PickleableCompModelBC, make_evaluate_b_candidate_kwargs, make_evaluate_b_candidates_kwargs, _get_plate_kwargs
 
-
-class PickleableSWIG(object):
-    # http://stackoverflow.com/a/9325185
-    def __setstate__(self, state):
-        print(state)
-        self.__init__(*state['args'])
-
-    def __getstate__(self):
-        return {'args': self.args}
-
-
-class PickleableCompModelBC(CompModelBC, PickleableSWIG):
-    # http://stackoverflow.com/a/9325185
-    def __init__(self, *args):
-        self.args = args
-        CompModelBC.__init__(self)
-
-
-class PickleablePlate(Plate, PickleableSWIG):
-    # http://stackoverflow.com/a/9325185
-    def __init__(self, *args):
-        self.args = args
-        Plate.__init__(self, **self.args[0])
-
-
-class PickleableRoadRunner(roadrunner.RoadRunner, PickleableSWIG):
-    # http://stackoverflow.com/a/9325185
-    def __init__(self, *args):
-        self.args = args
-        roadrunner.RoadRunner.__init__(self)
 
 
 # load plate data from json.
 with open("temp_sim_and_est_data.json", 'r') as f:
     # data = dict_to_numpy(json.load(f))
     data = dict_to_numpy(json.load(f))
-
-data["bounds"][4:] = np.array([0.0, 150])
 
 # Just use evolutionary strategy to get bs and supply true C_0, N_0
 # NE_0, and kn.
@@ -57,26 +24,28 @@ cols = data["cols"]
 no_cultures = rows*cols
 plate_lvl = data["sim_params"][:-no_cultures]
 
-bounds = data["bounds"][-no_cultures:]
 
-# model = CompModelBC()
-gen_kwargs = {
-    "bounds": bounds,
-}
-eval_kwargs = {
-    "plate_kwargs": get_plate_kwargs(data),
-    "plate_lvl": data["sim_params"][:-no_cultures],
-    "fitter": Fitter(),
-    "model": CompModelBC().name,
-    "plate": Plate(**get_plate_kwargs(data)),
-#    "model": CompModelBC(),
-}
+bounds = np.array([[0.0, 80.0] for i in range(no_cultures)])
 
+model = PickleableCompModelBC()
 args = {
-    "gen_kwargs": gen_kwargs,
-    "eval_kwargs": eval_kwargs,
+    "gen_kwargs": {"bounds": bounds},
+    "eval_kwargs": make_evaluate_b_candidates_kwargs(data, model, plate_lvl),
 }
-pickleable(args)
 
-# final_pop = mp_evolver(gen_random_uniform, evaluate_b_candidate, bounds, args,
-#                        cpus=4, pop_size=100, max_evals=1000, mut_rate=0.25)
+final_pop = evolver(gen_random_uniform, evaluate_b_candidates, bounds, args,
+                    pop_size=500, max_evals=100000)
+
+best = max(final_pop)
+print(best)
+est_params = np.concatenate((plate_lvl, best.candidate[:no_cultures]))
+print(est_params)
+
+true_plate = Plate(**_get_plate_kwargs(data))
+true_plate.sim_amounts = data["sim_amounts"]
+true_plate.sim_params = data["sim_params"]
+true_plate.set_rr_model(model, data["sim_params"])
+
+plotter = Plotter(model)
+plotter.plot_est_rr(true_plate, est_params, sim=True,
+                    title="Best Candidate Using Evolution Strategy")
