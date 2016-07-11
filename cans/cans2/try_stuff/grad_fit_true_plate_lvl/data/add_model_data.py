9e2c29d0cbadf8396eab4377a318c5c15235371b
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-11 16:56:17 +0100

    Add params not species twice

--- cans/cans2/try_stuff/grad_fit_true_plate_lvl/data/add_model_data.py
+++ cans/cans2/try_stuff/grad_fit_true_plate_lvl/data/add_model_data.py
@@ -1,9 +1,11 @@
 import json
 
+
 from cans2.model import CompModelBC
 from cans2.cans_funcs import cans_to_json, dict_to_json
 
-# rows, cols = (16, 24)
+
+# rows, cols = (3, 3)
 
 data_path = "sim_plate_with_fit_{0}x{1}.json".format(rows, cols)
 with open(data_path, "r") as f:
@@ -12,7 +14,7 @@ with open(data_path, "r") as f:
 model = CompModelBC()
 data["model"] = model.name
 data["species"] = model.species
-data["params"] = model.species
+data["params"] = model.params
 data = dict_to_json(data)
 
 with open(data_path, "w") as f:
