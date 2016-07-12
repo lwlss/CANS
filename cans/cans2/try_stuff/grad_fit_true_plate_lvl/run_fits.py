ff53cfdfbb7798f46a55df1d2e34f0dab847e207
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-12 19:49:08 +0100

    correct script name

--- cans/cans2/try_stuff/grad_fit_true_plate_lvl/run_fits.py
+++ cans/cans2/try_stuff/grad_fit_true_plate_lvl/run_fits.py
@@ -5,6 +5,6 @@ do
     for j in {0..1}
     do
 	# echo "$i$j"
-	python compare_guessed_and_uniform.py $i $j >/dev/null 2>&1 &
+	python compare_guessed_and_uniform_b.py $i $j >/dev/null 2>&1 &
     done
 done
