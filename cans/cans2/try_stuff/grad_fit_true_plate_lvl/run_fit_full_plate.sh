00a3ca94e089ed5c8d33606acfd12058128b294c
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-12 19:42:33 +0100

    rename bash and nest loops

--- cans/cans2/try_stuff/grad_fit_true_plate_lvl/run_fit_full_plate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {0..4}
-do
-    python compare_guessed_and_uniform.py $i 0 >/dev/null 2>&1 &
-    # python compare_guessed_and_uniform.py $i 1 >/dev/null 2>&1 &
-    # python fit_full_plate.py $i >/dev/null &
-done
---
+++ cans/cans2/try_stuff/grad_fit_true_plate_lvl/run_fits.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {0..4}
+do
+    for j in {0..1}
+    do
+	# echo "$i$j"
+	python compare_guessed_and_uniform.py $i $j >/dev/null 2>&1 &
+    done
+done
