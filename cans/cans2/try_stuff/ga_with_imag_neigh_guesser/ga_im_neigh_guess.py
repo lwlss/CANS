de043fd3f5988e649e85961a131fe82ff8e70fc6
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-14 16:45:03 +0100

    Running on 3x3

--- cans/cans2/try_stuff/ga_with_imag_neigh_guesser/ga_im_neigh_guess.py
+++ cans/cans2/try_stuff/ga_with_imag_neigh_guesser/ga_im_neigh_guess.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import inspyred
 
+
 import cans2.genetic2 as genetic
 import cans2.genetic_kwargs as kwargs
 
@@ -11,6 +12,7 @@ from cans2.cans_funcs import dict_to_numpy
 from cans2.model import CompModelBC
 from cans2.plate import Plate
 from cans2.plotter import Plotter
+from cans2.make_sbml import create_sbml
 
 
 model = kwargs.PickleableCompModelBC()    # Pickleable for multiprocessing.
@@ -22,7 +24,6 @@ seed, random = genetic.get_seed_and_prng(seed_file)
 
 # load plate data from json.
 with open("data/sim_and_est_data_3x3.json", 'r') as f:
-    # data = dict_to_numpy(json.load(f))
     data = dict_to_numpy(json.load(f))
 
 true_plate = Plate(**kwargs._get_plate_kwargs(data))
@@ -31,58 +32,48 @@ true_plate.sim_params = data["sim_params"]
 true_plate.set_rr_model(model, data["sim_params"])
 no_cultures = data["rows"]*data["cols"]
 
-# Check we have the correct parameters for the data.
+# # Check we have the correct parameters for the data.
 # plotter.plot_est_rr(true_plate, data["sim_params"], sim=False)
 
-# Prepare args.
-b_bounds = np.array([np.array([0.0, 100]) for i in range(no_cultures)])
-c_evolver_kwargs = {
-    "generator": genetic.gen_random_uniform,
-    "evaluator": genetic.eval_b_candidates,
-    "observer": [],    # Internal observer.
-    # "observer": [inspyred.ec.observers.stats_observer,
-    #              inspyred.ec.observers.best_observer],
-    "bounds": b_bounds,
-    # "args": None,    # To be set inside plate_level eval
-    "random": random,
-    "pop_size": 20,
-    "num_selected": 20,
-    "max_evals": 10000,
-    "mut_rate": 1.0,
-    "crowd_dist": 10,    # Must be integer.
-    }
-c_evolver = kwargs.package_evolver(genetic.custom_evolver, **c_evolver_kwargs)
-plate_lvl_bounds = data["bounds"][:model.b_index]
+gen_kwargs = {
+    # plate level and b_guess bounds
+    "bounds": np.concatenate((data["bounds"][:model.b_index], [[0.0, 100.0]]))
+}
+
+plate = Plate(**kwargs._get_plate_kwargs(data))
+
+eval_kwargs = {
+    "plate": plate,
+    "model": model,
+    "imag_neigh_params": np.array([1.0, 1.0, 0.0, 0.0, 0.0]),    # Last two are place holders for b_guess*1.5 and b_guess.
+    "b_bounds": np.array([[0.0, 200.0] for i in range(no_cultures)]),
+}
 args = {
-    "gen_kwargs": {"bounds": plate_lvl_bounds},
-    "eval_kwargs": kwargs.make_eval_plate_lvl_kwargs(data, model, c_evolver),
-    }
+    "gen_kwargs": gen_kwargs,
+    "eval_kwargs": eval_kwargs,
+}
 
 
 # Now call plate lvl parallel evolver.
-final_pop = genetic.custom_mp_evolver(generator=genetic.gen_random_uniform_log_C,
-                                      evaluator=genetic.eval_plate_lvl,
-                                      bounds=plate_lvl_bounds,
-                                      args=args,
-                                      random=random,
-                                      cpus=4,
-                                      pop_size=10,
-                                      num_selected=10,
-                                      max_evals=20,
-                                      mut_rate=1.0,
-                                      crowd_dist=10)    # Must be integer.
-
+final_pop = genetic.mp_evolver(generator=genetic.gen_random_uniform,
+                               evaluator=genetic.eval_plate_lvl_im_neigh_grad,
+                               bounds=args["gen_kwargs"]["bounds"],
+                               args=args,
+                               random=random,
+                               cpus=6,
+                               pop_size=6,
+                               max_evals=6*10)
 print(final_pop)
 
 best = max(final_pop)    # Always max even if you are minimizing objective function.
-est_plate_lvl = best.candidate[:model.b_index]
+est_plate_lvl = best.candidate[:model.b_index+1]
 
 print("best", est_plate_lvl, best.fitness)
 print("true", data["sim_params"])
 
-# Concatenate est with true. Should really get the parameters back
+# Concatenate est with true bs removing b_guess. Should really get the parameters back
 # from the best eval
-est_params = np.concatenate((est_plate_lvl, data["sim_params"][-no_cultures:]))
+est_params = np.concatenate((est_plate_lvl[:-1], data["sim_params"][-no_cultures:]))
 
 plotter.plot_est_rr(true_plate, est_params, sim=False,
                     title="Best Candidate Using Evolution Strategy")
