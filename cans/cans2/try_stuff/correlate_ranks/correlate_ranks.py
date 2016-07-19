185f7f8ace7a6ce543cb4526615f5d4f1bbe0f71
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-19 13:04:20 +0100

    treat CompModel and CompModelBC

--- cans/cans2/try_stuff/correlate_ranks/correlate_ranks.py
+++ cans/cans2/try_stuff/correlate_ranks/correlate_ranks.py
@@ -10,18 +10,26 @@ from cans2.process import find_best_fits
 
 
 best_no_bc = np.array(find_best_fits("../../results/p15_fits/full_plate/CompModel/*.json",
-                                     num=3, key="obj_fun"))
+                                     num=1, key="obj_fun"))
 best_bc = np.array(find_best_fits("../../results/p15_fits/full_plate/CompModelBC/*.json",
-                                  num=3, key="obj_fun"))
-print(best_bc)
+                                  num=1, key="obj_fun"))
+
 
 genes = get_genes("data/p15/ColonyzerOutput.txt")[:384]
 
-ests = []
+bc_ests = []
 for est in best_bc:
     with open(est[0], "r") as f:
-        ests.append(json.load(f)["comp_est"][4:])
+        bc_ests.append(json.load(f)["comp_est"][4:])
+
+no_bc_ests = []
+for est in best_no_bc:
+    with open(est[0], "r") as f:
+        no_bc_ests.append(json.load(f)["comp_est"][3:])
+
+
 
 
 correlate_ests(genes,
-               *[("CompModelBC_{0}".format(i), est) for i, est in enumerate(ests)])
+               *([("CompModelBC_{0}".format(i), est) for i, est in enumerate(bc_ests)] +
+                 [("CompModel_{0}".format(i), est) for i, est in enumerate(no_bc_ests)]))
