c2dd2f968c9b7b620a72860d4d0417ef24af230d
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-19 10:53:08 +0100

    Rank plotting

---
+++ cans/cans2/rank.py
@@ -0,0 +1,16 @@
+"""Plot correlations in b value rank between two estimates (or true if sim)."""
+# Coefficient of variation.
+import matplotlib.pyplot as plt
+
+
+
+def correlate_ests(genes, *ests):
+    """Plot correlations for multiple estimates and label with gene name.
+
+    genes : A list of gene names.
+
+    *ests : Corresponding parameter estimates. Each a tuple of
+    estimate label and list of values.
+
+    """
+    pass
