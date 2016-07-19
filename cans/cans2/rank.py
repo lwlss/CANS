8b02b10843511ea9833afafc79139287e7337545
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-19 12:15:09 +0100

    Have ranks now

--- cans/cans2/rank.py
+++ cans/cans2/rank.py
@@ -1,7 +1,14 @@
 """Plot correlations in b value rank between two estimates (or true if sim)."""
 # Coefficient of variation.
+import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.stats import rankdata
+from matplotlib.cm import coolwarm
+
+
+def rank_bs(est):
+    rank_data(est)
 
 
 def correlate_ests(genes, *ests):
@@ -13,4 +20,13 @@ def correlate_ests(genes, *ests):
     estimate label and list of values.
 
     """
-    pass
+    labels = [est[0] for est in ests]
+
+    ranked = np.array([rankdata(est[1]) for est in ests])
+    ranks = np.array([ranked[:, i] for i in range(len(genes))])
+
+
+    fig = plt.figure(facecolor="white")
+    ax = plt.axes(frameon=False)
+    ax.get_xaxis().tick_bottom()
+    cols = coolwarm(np.linspace(0, 1, len(genes)))
