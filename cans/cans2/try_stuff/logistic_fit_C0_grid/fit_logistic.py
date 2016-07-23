21719ccefe197218c7cb8e84d4e4fef961103771
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-23 23:10:41 +0100

    Read in p15 and make plate

--- cans/cans2/try_stuff/logistic_fit_C0_grid/fit_logistic.py
+++ cans/cans2/try_stuff/logistic_fit_C0_grid/fit_logistic.py
@@ -2,22 +2,25 @@ import numpy as np
 import sys
 
 
-from cans2.parser import
+from cans2.parser import get_plate_data
+from cans2.plate import Plate
+from cans2.guesser import fit_log_eq
 
 
-index = sys.argv[1]
+C_0s_index = int(sys.argv[1])
 
 all_C_0s = np.logspace(-6, -3, 1000)
-C_0s = all_C_0[i:i+20]
+C_0s = all_C_0s[C_0s_index:C_0s_index+20]
 
 
 # read in plate 15 data and make a plate.
-p15_dir = "
+data_path = "../../../../data/p15/Output_Data/"
+plate_data = get_plate_data(data_path)
+plate = Plate(plate_data["rows"], plate_data["cols"],
+                   data=plate_data)
 
 
 for C_0 in C_0s:
 
-    params =
-    model =
-
-    plate =
+    #fit_log_eq(
+    pass
