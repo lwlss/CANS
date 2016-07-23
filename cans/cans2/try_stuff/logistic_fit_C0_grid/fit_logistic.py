4a66e29838f3f2b0bad6180edead1bef088d0958
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-23 23:01:44 +0100

    initiate script to run logistic fits w/ C0 fixed

---
+++ cans/cans2/try_stuff/logistic_fit_C0_grid/fit_logistic.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+
+
+from cans2.parser import
+
+
+index = sys.argv[1]
+
+all_C_0s = np.logspace(-6, -3, 1000)
+C_0s = all_C_0[i:i+20]
+
+
+# read in plate 15 data and make a plate.
+p15_dir = "
+
+
+for C_0 in C_0s:
+
+    params =
+    model =
+
+    plate =
