06c3caa1caf1559ad7013fab90d996220235b122
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-25 01:03:11 +0100

    Plot best log eq fit

--- cans/cans2/try_stuff/logistic_fit_C0_grid/find_and_check_best_fits.py
+++ cans/cans2/try_stuff/logistic_fit_C0_grid/find_and_check_best_fits.py
@@ -17,10 +17,10 @@ from cans2.genetic_kwargs import _get_plate_kwargs
 log_eq_model = IndeModel()
 
 # genes = get_genes("data/p15/ColonyzerOutput.txt")[:384]
-log_path = "results/log_eq_fit_*.json"
+log_path = "results2/*.json"
 comp_bc_path = "../../results/p15_fits/full_plate/CompModelBC/*.json"
 
-best_logs = np.array(find_best_fits(log_path, num=3, key="obj_fun_internals"))
+best_logs = np.array(find_best_fits(log_path, num=3, key="obj_funs_internals"))
 best_comps = np.array(find_best_fits(comp_bc_path, num=1, key="obj_fun"))
 
 # Remove edge cultures from obj_fun total for logistic equivalent model. Open all of the files and resave
@@ -28,10 +28,10 @@ best_comps = np.array(find_best_fits(comp_bc_path, num=1, key="obj_fun"))
 print(best_logs[:,])
 
 
-log_ests = []
-for est in best_logs:
-    with open(est[0], "r") as f:
-        bc_ests.append(json.load(f)["comp_est"][4:])
+# log_ests = []
+# for est in best_logs:
+#     with open(est[0], "r") as f:
+#         log_ests.append(json.load(f)["_est"][4:])
 
 
 log_data = read_in_json(best_logs[0][0])
@@ -43,7 +43,6 @@ for culture, log_est in zip(log_plate.cultures, log_data["culture_est_params"]):
     culture.log_est = log_est
 
 plotter = Plotter(log_eq_model)
-for fit in best_logs[:3]
 plotter.plot_culture_fits(log_plate, log_eq_model,
                           title="Best Logistic Equivalent Fit",
                           est_name="log_est")
