1e7e32c44bc008c49438af20568ee710aaaa3d10
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-28 15:17:22 +0100

    Fit now works

--- cans/cans2/try_stuff/stripes/spline.py
+++ cans/cans2/try_stuff/stripes/spline.py
@@ -16,7 +16,7 @@ from scipy import interpolate
 
 from cans2.parser import get_plate_data2
 from cans2.plate import Plate
-from cans2.model import CompModelBC
+from cans2.model import CompModelBC, CompModel
 # from cans2.guesser import fit_imag_neigh, Guesser
 # from cans2.cans_funcs import dict_to_json
 from cans2.plotter import Plotter
@@ -30,7 +30,7 @@ barcodes = np.array([
 ])
 barcode = barcodes[int(sys.argv[2])]
 
-plate_model = CompModelBC()
+plate_model = CompModel()
 plotter = Plotter(plate_model)
 
 # Read in real data and make a plate.
@@ -53,26 +53,55 @@ print(zone)
 custom_params = {
     "C_0": 1e-4,
     "N_0": 0.1,
-    "NE_0": 0.15,
-    "kn": 6.0,
+#    "NE_0": 0.15,
+    "kn": 2.0,
     }
 
 sim_3x3 = Plate(3, 3)
 sim_3x3.times = zone.times
+sim_3x3.growers = np.arange(sim_3x3.no_cultures)
+sim_3x3.empties = []
 sim_3x3.set_sim_data(plate_model, b_mean=35.0, b_var=15.0,
                      custom_params=custom_params, noise=True)
-# plotter.plot_c_meas(sim_3x3)
+# plotter.plot_c_meas(sim_3x3)    # Plot Spline would be nice.
 
-bounds = np.array([[p/2.0, p*2.0] for p in sim_3x3.sim_params])
-param_guess = sim_3x3.sim_params
-zone.make_spline(time_steps=15)
-zone.set_rr_model(plate_model, param_guess)
+bounds = [
+    [1e-4, 1e-4],
+    [0.1, 0.1],
+#    [0.085, 0.2],
+    [2.0, 2.0],
+    ]
+bounds += [[0.0, 100.0] for i in range(sim_3x3.no_cultures)]
+bounds = np.array(bounds)
+
+param_guess = np.concatenate((np.array([1e-4, 0.1, 2.0]),
+                              np.array([35.0 for i in range(sim_3x3.no_cultures)])))
+
+
+print(bounds)
+print(param_guess)
+
+sim_3x3.make_spline(time_steps=15)
+sim_3x3.set_rr_model(plate_model, param_guess)
+
+assert len(sim_3x3.c_spline) == len(sim_3x3.t_spline)*sim_3x3.no_cultures
+
+plotter.plot_spline(sim_3x3)
+
 # Default smoothing s=m-sqrt(2*m) where m is the number of data points.
-est = zone.fit_spline(plate_model, param_guess, bounds,
-                      minimizer_opts={"disp": True})
+est = sim_3x3.fit_spline(plate_model, param_guess, bounds,
+                         minimizer_opts={"disp": True})
 
 
-plotter.plot_est_rr(zone, est.x, title="Zone spline growth")
+print("truth")
+print(sim_3x3.sim_params)
+print("est")
+print(est.x)
+
+plotter.plot_est_rr(sim_3x3, est.x, title="Zone spline growth")
+
+
+
 # plt.figure()
 # plt.plot(zone.t_spline, zone.c_spline, 'x', xnew, ynew, xnew, np.sin(xnew), x, y, 'b')
 # plt.legend(['Linear', 'Cubic Spline', 'True'])
