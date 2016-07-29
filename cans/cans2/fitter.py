80ac32b39760bf19a7a95d0f4397878a2be7ad0f
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-29 12:01:44 +0100

    Note for when refactoring on a better way.

--- cans/cans2/fitter.py
+++ cans/cans2/fitter.py
@@ -55,6 +55,12 @@ class Fitter(object):
         # Mutable so must scale C_0 back
         params[0] = params[0]*10000
         c_est = np.split(amount_est, self.model.no_species, axis=1)[0]
+        # CONSIDER WHEN REFACTORING. It will probably quicker to
+        # remove the cell reaction equations for empty cultures from
+        # the SMBL. Then, cell amounts will not be returned by
+        # rr_solve for empty cultures and we do not have to slice by
+        # the indices of all of the growers. Instead we would slice as
+        # cest[:, :len(plate.growers)].
         growers_c_est = c_est[:, list(plate.growers)].flatten()
         err = np.sqrt(np.sum((plate.c_meas_obj - growers_c_est)**2))
         return err
