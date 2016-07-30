a100e38d68ddc88ccd6d78f893c02221fca14254
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-30 18:12:38 +0100

    Spearmans for top five in each plate

--- cans/cans2/try_stuff/stripes/comp_model_bc_spline/spearmans.py
+++ cans/cans2/try_stuff/stripes/comp_model_bc_spline/spearmans.py
@@ -1,13 +1,24 @@
 import numpy as np
 import json
+import csv
 import Bio.Cluster
+
 
 
 from cans2.model import CompModelBC
 from cans2.process import find_best_fits, test_bounds, spearmans_rho, mad_tril
 
+bc_index = 0    # 0 for Stripe, 1 for filled.
+num_tops = 5    # Number of top fits to look at.
 
-best_fits = np.array(find_best_fits("full_plate/*/*.json", num=5, key="obj_fun"))
+barcodes = np.array([
+    {"barcode": "K000343_027_001", "ignore_empty": False, "name": "Empty"},
+    {"barcode": "K000347_027_022", "ignore_empty": True, "name": "Filled"},    # Filled stripes do not have correct gene names.
+])
+barcode = barcodes[bc_index]["barcode"]
+res_path = barcode + "/results/*.json"
+
+best_fits = np.array(find_best_fits(res_path, num=num_tops, key="obj_fun"))
 
 data = []
 for filename in best_fits[:, 0]:
@@ -15,21 +26,13 @@ for filename in best_fits[:, 0]:
         d = json.load(f)
     data.append(d)
 
-at_bounds = [any(test_bounds(d["comp_est"][:4], d["bounds"][:4])) for d in data]
+at_bounds = [any(test_bounds(d["est_params"][:4], d["bounds"][:4])) for d in data]
 print(at_bounds)
-
-guess_key = "guess_method_C_ratio_zero_kn"
-guess_method = [str(d[guess_key][0]) for d in data]
-print(guess_method)
-
-# Want to find the model associated with each fit and
-models = [d["model"] for d in data]
-print(models)
-
 
 # Make a matrix of correlations of rank orders.
 no_cultures = 16*24
-b_ests = np.array([d["comp_est"][-no_cultures:] for d in data])
+b_ests = np.array([d["est_params"][-no_cultures:] for d in data])
+plate_lvl_ests = np.array([d["est_params"][:-no_cultures] for d in data])
 
 distances = (spearmans_rho(b_ests))
 print(distances)
@@ -42,15 +45,15 @@ print(d_tri)
 mads = mad_tril(b_ests)
 print(mads)
 
-
-import csv
-outfile = "full_plate/replots/first_10_params.csv"
+outfile = barcode + "_spearmans.csv"
 with open(outfile, 'ab') as f:
     writer = csv.writer(f, delimiter="\t")
-    writer.writerow(models)
-    writer.writerow(["Spearmans for top 5 fits (either model)."])
+    writer.writerow(["Spearmans for top {0} fits {1}.".format(num_tops, barcode)])
     for r in d_tri:
         writer.writerow(r)
-    writer.writerow(["b parameter MADs for top 5 fits (either model)."])
+    writer.writerow(["b parameter MADs for top {0} fits {1}.".format(num_tops, barcode)])
     for r in mads:
+        writer.writerow(r)
+    writer.writerow(["Plate level parameter ests for top {0} fits {1}.".format(num_tops, barcode)])
+    for r in plate_lvl_ests:
         writer.writerow(r)
