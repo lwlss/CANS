6272aff88f8aff67d68c919e62a3aea8023959cd
Dan Boo <daniel.boocock@protonmail.ch>
2016-07-31 13:45:21 +0100

    Comparing vars

--- cans/cans2/try_stuff/correlate_ranks/correlate_ranks_QFA_R.py
+++ cans/cans2/try_stuff/correlate_ranks/correlate_ranks_QFA_R.py
@@ -4,7 +4,7 @@ import json
 
 from cans2.rank import correlate_ests, correlate_avgs, write_stats, mdr, mdp, mdrmdp, get_repeat_stats, get_c_of_v, plot_c_of_v
 from cans2.cans_funcs import dict_to_numpy
-from cans2.parser import get_genes, get_mdrmdp
+from cans2.parser import get_genes, get_mdrmdp, get_genes
 from cans2.process import find_best_fits, remove_edges
 
 
@@ -15,13 +15,15 @@ best_no_bc = np.array(find_best_fits("../../results/p15_fits/full_plate/CompMode
 best_bc = np.array(find_best_fits("../../results/p15_fits/full_plate/CompModelBC/*.json",
                                   num=1, key="obj_fun"))
 log_path = "../logistic_fit_C0_grid/results2/log_eq*.json"
-best_logs = np.array(find_best_fits(log_path, num=1, key="obj_funs_internals"))
+best_logs = np.array(find_best_fits(log_path, num=0, key="obj_funs_internals"))
 
 # Read in MDR*MDP from QFA R output.
 qfa_R_data = "data/p15/P15_QFA_GeneralisedLogisticFitnesses.txt"
 gen_log_mdr_mdp = get_mdrmdp(qfa_R_data)
 gen_log_ests = [gen_log_mdr_mdp]
-assert all(genes == get(genes(qfa_R_data)))
+assert all(genes == get_genes(qfa_R_data))
+
+rows, cols = 16, 24
 
 log_eq_ests = []
 log_eq_rs = []
@@ -66,7 +68,7 @@ gen_log_ests = [["Generalised Logistic", est] for est in gen_log_ests]
 log_eq_ests = [["Logistic Eq. Model".format(i), est] for i, est in enumerate(log_eq_ests)]
 
 
-ests = bc_ests + no_bc_ests + gen_log_ests + log_eq_ests
+ests = bc_ests + no_bc_ests + log_eq_ests + gen_log_ests
 
 # # Plot all genes
 # gene_set = set(genes)
@@ -80,7 +82,7 @@ ests = bc_ests + no_bc_ests + gen_log_ests + log_eq_ests
 # correlate_avgs(genes, "best_comp_bc_and_log_eq_cor.pdf", *ests)
 correlate_avgs(genes, "", *ests)
 
-assert False
+#assert False
 # Now get the coefficient of variation for best bc_est and log_eq_est
 plot_c_of_v(genes, *ests)
 
