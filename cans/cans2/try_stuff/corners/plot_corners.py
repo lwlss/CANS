34960eecda9c5cc389cdb7f165a4f93422176c73
Dan Boo <daniel.boocock@protonmail.ch>
2016-08-04 15:37:58 +0100

    Plots show only sim for one param set

--- cans/cans2/try_stuff/corners/plot_corners.py
+++ cans/cans2/try_stuff/corners/plot_corners.py
@@ -6,44 +6,26 @@ import json
 from cans2.plotter import Plotter
 from cans2.model import CompModel, CompModelBC
 from cans2.plate import Plate
-from cans2.process import find_best_fits
+from cans2.process import find_best_fits, read_in_json
+from cans2.genetic_kwargs import _get_plate_kwargs
 
-
-comp_re = "../../results/p15_fits/full_plate/CompModel/*.json"
-comp_file = data_file.format(argv, b_guess)
-
-comp_bc_re = "../../results/p15_fits/full_plate/CompModelBC/*.json"
-data_file = data_file.format(argv, b_guess)
-
-result_path = "full_plate/CompModelBC/*.json"
-best_fit = find_best_fits(result_path, num=1, key="obj_fun")
-
-print(best_fit)
-
-# Read in data from json file
-with open(best_fit[0][0], 'r') as f:
-    fit_data = json.load(f)
-
-
-plate = Plate(fit_data["rows"], fit_data["cols"])
+paths = [
+    "../../results/p15_fits/full_plate/CompModel/*.json",
+    "../../results/p15_fits/full_plate/CompModelBC/*.json",
+]
+fit_data = [read_in_json(find_best_fits(p, 1, "obj_fun")[0][0]) for p in paths]
+plates = [Plate(**_get_plate_kwargs(data)) for data in fit_data]
 models = [CompModel(), CompModelBC()]
-model_name = fit_data["model"]
-model = next((m for m in models if m.name == fit_data["model"]), None)
 
-# Add necessary data attributes to produce plots
-plate.times = fit_data['times']
-plate.c_meas = fit_data['c_meas']
-# plate.sim_params = fit_data['comp_est']
-plate.set_rr_model(model, fit_data['comp_est'])
-plate.sim_params = fit_data["comp_est"]
+for plate, data in zip(plates, fit_data):
+    plate.est_params = data["comp_est"]
+est_params = [plate.est_params for plate in plates]
 
-
-# coords = (0, 0)
-# rows, cols = 3, 3
-models = [CompModel(), CompModelBC()]
-# plotter = Plotter(model, lw=3.0, ms=10.0, mew=2.0, xpad=15, ypad=30)
-# plot_title = r"Best Competition Model BC Fit to \textit{cdc13-1} P15 at 27C (R5, C18)"
-# plotter.plot_zone_est([plate], [""],
-#                       [plate.sim_params], models, coords,
-#                       rows, cols, legend=True, title=plot_title,
-#                       plot_types=["Est."], vis_ticks=True)
+coords = (0, 0)
+rows, cols = 3, 3
+plotter = Plotter(CompModelBC() , lw=3.0, ms=10.0, mew=2.0, xpad=15, ypad=30)
+plot_title = r"Best Competition Fits to Corner of \textit{cdc13-1} P15 at 27C"
+plotter.plot_zone_est(plates, ["Comp. Model", "Comp. Model BC"],
+                      est_params, models, coords, rows, cols,
+                      legend=True, title=plot_title,
+                      plot_types=["Est."], vis_ticks=True)
